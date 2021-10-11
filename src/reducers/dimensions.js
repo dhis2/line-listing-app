6bd89ea5cf5a98bc0ad3c128383e4f5b2faf533f
Martin Ohlson <martin@moid.se>
2021-10-11 10:34:24 +0200

    chore: remove unusual docs

--- src/reducers/dimensions.js
+++ src/reducers/dimensions.js
@@ -18,17 +18,4 @@ export default (state = DEFAULT_DIMENSIONS, action) => {
     }
 }
 
-// Selectors
-
-/**
- * Selector which returns dimensions from the state object
- * If state.dimensions is null, then the dimensions api request
- * has not yet completed. If state.dimensions is an empty object
- * then the dimensions api request is complete, and there are no
- * dimensions
- *
- * @function
- * @param {Object} state The current state
- * @returns {Array}
- */
 export const sGetDimensions = state => state.dimensions
