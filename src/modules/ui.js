f1243f9a19ac574ffb3cf8e3b20b9025afc66a4f
Jan Henrik Øverland <jan@dhis2.org>
2021-12-29 18:42:51 +0100

    fix: repetition format parser (#888)
    * parse repetition WIP
    
    * parser fns and tests
    
    * check if input is modified
    
    * remove old file
    
    * improve validation
    
    * fix props

--- src/modules/ui.js
+++ src/modules/ui.js
@@ -3,6 +3,7 @@ import {
     layoutGetAxisIdDimensionIdsObject,
     layoutGetDimensionIdItemIdsObject,
 } from '@dhis2/analytics'
+import isObject from 'lodash-es/isObject'
 import { getAdaptedUiLayoutByType, getInverseLayout } from './layout.js'
 import { getOptionsFromVisualization } from './options.js'
 import { removeLastPathSegment } from './orgUnit.js'
@@ -78,3 +79,62 @@ const getConditionsFromVisualization = (vis) =>
             }),
             {}
         )
+
+// Repetition
+
+export const PROP_MOST_RECENT = 'mostRecent'
+export const PROP_OLDEST = 'oldest'
+
+export const getDefaultCurrentRepetition = () => [0]
+export const getDefaultUiRepetition = () => ({
+    [PROP_MOST_RECENT]: 1,
+    [PROP_OLDEST]: 0,
+})
+
+export const PARSE_CURRENT_REPETITION_ERROR =
+    'parseCurrentRepetition: Invalid input'
+
+export const PARSE_UI_REPETITION_ERROR = 'parseUiRepetition: Invalid input'
+
+export const parseCurrentRepetition = (repetition) => {
+    if (
+        !(
+            Array.isArray(repetition) &&
+            repetition.length &&
+            repetition.every((i) => typeof i === 'number')
+        )
+    ) {
+        throw new Error(PARSE_CURRENT_REPETITION_ERROR)
+    }
+
+    return {
+        [PROP_MOST_RECENT]: repetition.filter((n) => n < 1).length,
+        [PROP_OLDEST]: repetition.filter((n) => n > 0).length,
+    }
+}
+
+export const parseUiRepetition = (repetition) => {
+    if (
+        !(
+            isObject(repetition) &&
+            typeof repetition[PROP_MOST_RECENT] === 'number' &&
+            repetition[PROP_MOST_RECENT] >= 0 &&
+            typeof repetition[PROP_OLDEST] === 'number' &&
+            repetition[PROP_OLDEST] >= 0
+        )
+    ) {
+        throw new Error(PARSE_UI_REPETITION_ERROR)
+    }
+
+    if (repetition[PROP_MOST_RECENT] === 0 && repetition[PROP_OLDEST] === 0) {
+        return getDefaultCurrentRepetition()
+    }
+
+    return [
+        ...new Array(repetition[PROP_OLDEST]).fill().map((_, i) => i + 1),
+        ...new Array(repetition[PROP_MOST_RECENT])
+            .fill()
+            .map((_, i) => -i + 0)
+            .sort((a, b) => a - b),
+    ]
+}
