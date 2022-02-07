f159d341d30042d70585a892b3b5d36ca354c8b1
Hendrik de Graaf <hendrik@dhis2.org>
2022-02-07 15:39:44 +0100

    feat: add main dimensions section

--- src/components/MainSidebar/MainDimensions.js
+++ src/components/MainSidebar/MainDimensions.js
@@ -1,48 +1,82 @@
 import i18n from '@dhis2/d2-i18n'
-import React from 'react'
-// import { useSelector } from 'react-redux'
-// import {
-//     DIMENSION_TYPE_EVENT_DATE,
-//     DIMENSION_TYPE_ENROLLMENT_DATE,
-//     DIMENSION_TYPE_INCIDENT_DATE,
-//     DIMENSION_TYPE_SCHEDULED_DATE,
-//     DIMENSION_TYPE_LAST_UPDATED,
-// } from '../../modules/dimensionTypes.js'
-// import { OUTPUT_TYPE_EVENT } from '../../modules/visualization.js'
-// import {
-//     sGetUiInputType,
-//     sGetUiProgramId,
-//     sGetUiProgramStageId,
-// } from '../../reducers/ui.js'
-// import { DimensionItem } from './DimensionItem/index.js'
-// import { PROGRAM_TYPE_WITH_REGISTRATION } from './ProgramDimensionsPanel/ProgramDimensionsPanel.js'
-// import { DimensionItem } from './DimensionItem/index.js'
+import React, { useMemo } from 'react'
+import { useSelector, useStore } from 'react-redux'
+import {
+    DIMENSION_TYPE_OU,
+    DIMENSION_TYPE_CREATED_BY,
+    DIMENSION_TYPE_PROGRAM_STATUS,
+    DIMENSION_TYPE_EVENT_STATUS,
+    DIMENSION_TYPE_LAST_UPDATED_BY,
+} from '../../modules/dimensionTypes.js'
+import { OUTPUT_TYPE_ENROLLMENT } from '../../modules/visualization.js'
+import { sGetUiProgramId, sGetUiInputType } from '../../reducers/ui.js'
 import { DimensionItem } from './DimensionItem/index.js'
 import { MainSidebarSection } from './MainSidebarSection.js'
-// import { useSelectedDimensions } from './SelectedDimensionsContext.js'
+import { PROGRAM_TYPE_WITHOUT_REGISTRATION } from './ProgramDimensionsPanel/ProgramDimensionsPanel.js'
+import { useSelectedDimensions } from './SelectedDimensionsContext.js'
+
+const useProgramType = () => {
+    const store = useStore()
+    const programId = useSelector(sGetUiProgramId)
+
+    return useMemo(() => {
+        if (!programId) {
+            return null
+        }
+
+        const { metadata } = store.getState()
+        return metadata[programId].programType
+    }, [programId])
+}
 
 export const MainDimensions = () => {
-    // const { getIsDimensionSelected } = useSelectedDimensions()
-    // const selectedInputType = useSelector(sGetUiInputType)
-    // const programId = useSelector(sGetUiProgramId)
-    // const stageId = useSelector(sGetUiProgramStageId)
+    const inputType = useSelector(sGetUiInputType)
+    const programType = useProgramType()
+    const { getIsDimensionSelected } = useSelectedDimensions()
+    const allDisabled = !programType || !inputType
 
     return (
         <MainSidebarSection header={i18n.t('Main dimensions')}>
             <DimensionItem
-                dimensionType={'test'}
-                name={'test'}
-                id={1}
-                selected={true}
-                disabled={false}
-                optionSet={undefined}
-                valueType={'what'}
+                dimensionType={DIMENSION_TYPE_OU}
+                name={i18n.t('Organisation unit')}
+                id={DIMENSION_TYPE_OU}
+                selected={getIsDimensionSelected(DIMENSION_TYPE_OU)}
+                disabled={allDisabled}
+            />
+            <DimensionItem
+                dimensionType={DIMENSION_TYPE_PROGRAM_STATUS}
+                name={i18n.t('Program status')}
+                id={DIMENSION_TYPE_PROGRAM_STATUS}
+                selected={getIsDimensionSelected(DIMENSION_TYPE_PROGRAM_STATUS)}
+                disabled={
+                    allDisabled ||
+                    programType === PROGRAM_TYPE_WITHOUT_REGISTRATION
+                }
+            />
+            <DimensionItem
+                dimensionType={DIMENSION_TYPE_EVENT_STATUS}
+                name={i18n.t('Event status')}
+                id={DIMENSION_TYPE_EVENT_STATUS}
+                selected={getIsDimensionSelected(DIMENSION_TYPE_EVENT_STATUS)}
+                disabled={allDisabled || inputType === OUTPUT_TYPE_ENROLLMENT}
+            />
+            <DimensionItem
+                dimensionType={DIMENSION_TYPE_CREATED_BY}
+                name={i18n.t('Created by')}
+                id={DIMENSION_TYPE_CREATED_BY}
+                selected={getIsDimensionSelected(DIMENSION_TYPE_CREATED_BY)}
+                disabled={allDisabled}
+            />
+            <DimensionItem
+                dimensionType={DIMENSION_TYPE_LAST_UPDATED_BY}
+                name={i18n.t('Last updated by')}
+                id={DIMENSION_TYPE_LAST_UPDATED_BY}
+                selected={getIsDimensionSelected(
+                    DIMENSION_TYPE_LAST_UPDATED_BY
+                )}
+                disabled={allDisabled}
             />
         </MainSidebarSection>
     )
-    // return (
-    //     <MainSidebarSection header={i18n.t('Main dimensions')}>
-    //         <h1>Henkie</h1>
-    //     </MainSidebarSection>
-    // )
 }
