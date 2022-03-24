5090ea520e15d11d3604152378f006ec6db69346
Jen Jones Arnesen <jennifer@dhis2.org>
2022-03-24 12:13:47 +0100

    fix: hoist error check from Chip to DefaultAxis

--- src/components/Layout/Chip.js
+++ src/components/Layout/Chip.js
@@ -7,7 +7,6 @@ import PropTypes from 'prop-types'
 import React, { useMemo, useCallback } from 'react'
 import { useSelector, useDispatch } from 'react-redux'
 import { acSetUiOpenDimensionModal } from '../../actions/ui.js'
-import { sGetLoadError } from '../../reducers/loader.js'
 import { sGetMetadata } from '../../reducers/metadata.js'
 import {
     sGetUiItemsByDimension,
@@ -36,8 +35,6 @@ const Chip = ({ dimension, axisId, isLast, overLastDropZone, activeIndex }) => {
         [id, name, dimensionType, valueType, optionSet]
     )
 
-    const metadata = useSelector(sGetMetadata)
-
     const {
         attributes,
         listeners,
@@ -52,12 +49,10 @@ const Chip = ({ dimension, axisId, isLast, overLastDropZone, activeIndex }) => {
         id,
         data: memoizedDimensionMetadata,
     })
-    const globalLoadError = useSelector(sGetLoadError)
-
+    const metadata = useSelector(sGetMetadata)
     const conditions = useSelector((state) =>
         sGetUiConditionsByDimension(state, id)
     )
-
     const items = useSelector((state) => sGetUiItemsByDimension(state, id))
 
     const memoizedOnClick = useCallback(
@@ -99,10 +94,6 @@ const Chip = ({ dimension, axisId, isLast, overLastDropZone, activeIndex }) => {
     const dataTest = `layout-chip-${id}`
 
     const renderTooltipContent = () => <TooltipContent dimension={dimension} />
-
-    if (globalLoadError && !name) {
-        return null
-    }
 
     return (
         <div
