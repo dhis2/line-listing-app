fe6593b1be70bdff907c3a73303d81e5ab8ca790
Edoardo Sabadelli <edoardo@dhis2.org>
2022-04-13 12:02:14 +0200

    fix: fix vertical alignment of interpretation action buttons TECH-1085

--- src/components/Interpretations/common/Message/MessageIconButton.js
+++ src/components/Interpretations/common/Message/MessageIconButton.js
@@ -12,35 +12,48 @@ const MessageIconButton = ({
     iconComponent: Icon,
 }) => (
     <Tooltip closeDelay={200} content={tooltipContent}>
-        <button
-            onClick={(event) => {
-                event.stopPropagation()
-                onClick()
-            }}
-            className={cx('button', { selected })}
-            disabled={disabled}
-        >
-            {count && count}
-            <Icon color={selected ? colors.teal500 : colors.grey700} />
-        </button>
-        <style jsx>{`
-            .button {
-                all: unset;
-                cursor: pointer;
-                display: inline-flex;
-                flex-direction: row;
-                gap: ${spacers.dp4};
-                align-items: center;
-                font-size: 12px;
-                line-height: 14px;
-                color: ${colors.grey700};
-            }
+        {({ ref, onMouseOver, onMouseOut }) => (
+            <span
+                ref={ref}
+                onMouseOver={onMouseOver}
+                onMouseOut={onMouseOut}
+                className="wrapper"
+            >
+                <button
+                    onClick={(event) => {
+                        event.stopPropagation()
+                        onClick()
+                    }}
+                    className={cx('button', { selected })}
+                    disabled={disabled}
+                >
+                    {count && count}
+                    <Icon color={selected ? colors.teal500 : colors.grey700} />
+                </button>
+                <style jsx>{`
+                    .wrapper {
+                        display: inline-flex;
+                    }
 
-            .button.selected {
-                color: ${colors.teal600};
-                font-weight: 500;
-            }
-        `}</style>
+                    .button {
+                        all: unset;
+                        cursor: pointer;
+                        display: inline-flex;
+                        flex-direction: row;
+                        gap: ${spacers.dp4};
+                        align-items: center;
+                        font-size: 12px;
+                        line-height: 14px;
+                        color: ${colors.grey700};
+                    }
+
+                    .button.selected {
+                        color: ${colors.teal600};
+                        font-weight: 500;
+                    }
+                `}</style>
+            </span>
+        )}
     </Tooltip>
 )
 
