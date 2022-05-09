23454e63b25f1e96ea3bfc80d82223e3f9c316f9
Edoardo Sabadelli <edoardo@dhis2.org>
2022-05-09 15:18:22 +0200

    fix: add z-index on overlay to avoid buttons to stick out

--- src/components/Interpretations/InterpretationsUnit/InterpretationsUnit.js
+++ src/components/Interpretations/InterpretationsUnit/InterpretationsUnit.js
@@ -145,6 +145,7 @@ export const InterpretationsUnit = forwardRef(
                         display: flex;
                         align-items: center;
                         justify-content: center;
+                        z-index: 1;
                     }
 
                     .expanded {
