72260e2ead970051c72e58d7885e71a29c26fd42
Austin McGee <947888+amcgee@users.noreply.github.com>
2022-05-18 10:35:17 +0200

    fix: set min dhis2 version

--- d2.config.js
+++ d2.config.js
@@ -2,6 +2,8 @@ const config = {
     type: 'app',
     name: 'line-listing',
     title: 'Line Listing',
+    
+    minDHIS2Version: '2.38',
 
     entryPoints: {
         app: './src/AppWrapper.js',
