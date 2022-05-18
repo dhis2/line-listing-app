b6012745d1770bf3ff578ad012005c5c0a3307f1
Austin McGee <947888+amcgee@users.noreply.github.com>
2022-05-18 10:44:31 +0200

    chore: configure manifest for continuous delivery

--- d2.config.js
+++ d2.config.js
@@ -1,8 +1,9 @@
 const config = {
     type: 'app',
     name: 'line-listing',
+    id: 'a4cd3827-e717-4e09-965d-ab05df2591e5',
     title: 'Line Listing',
-    
+
     minDHIS2Version: '2.38',
 
     entryPoints: {
