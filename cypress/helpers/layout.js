76e3eb55fd59011dcdc5a5f72df42edc8efb315b
martinkrulltott <martin@moid.se>
2022-08-04 11:50:18 +0200

    test: refactor ..El to ..Selector

--- cypress/helpers/layout.js
+++ cypress/helpers/layout.js
@@ -1,10 +1,10 @@
-const getAxisEl = (axisId) => `${axisId}-axis`
-const getDimensionChipEl = (dimensionId) => `layout-chip-${dimensionId}`
+const getAxisSelector = (axisId) => `${axisId}-axis`
+const getDimensionChipSelector = (dimensionId) => `layout-chip-${dimensionId}`
 
 export const expectAxisToHaveDimension = (axisId, dimensionId) => {
     if (axisId && dimensionId) {
-        cy.getBySel(getAxisEl(axisId))
-            .findBySel(getDimensionChipEl(dimensionId))
+        cy.getBySel(getAxisSelector(axisId))
+            .findBySel(getDimensionChipSelector(dimensionId))
             .should('have.length', 1)
             .and('be.visible')
     } else {
@@ -14,8 +14,8 @@ export const expectAxisToHaveDimension = (axisId, dimensionId) => {
 
 export const expectAxisToNotHaveDimension = (axisId, dimensionId) => {
     if (axisId && dimensionId) {
-        cy.getBySel(getAxisEl(axisId))
-            .findBySel(getDimensionChipEl(dimensionId))
+        cy.getBySel(getAxisSelector(axisId))
+            .findBySel(getDimensionChipSelector(dimensionId))
             .should('not.exist')
     } else {
         throw new Error('axisId and dimensionId not provided')
