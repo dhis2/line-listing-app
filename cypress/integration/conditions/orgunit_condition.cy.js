3c2086909b9a13673f19efce498f0ce85fca93f6
martinkrulltott <martin@moid.se>
2022-08-10 09:44:17 +0200

    test: small refactor of org unit conds

--- cypress/integration/conditions/orgunit_condition.cy.js
+++ cypress/integration/conditions/orgunit_condition.cy.js
@@ -31,16 +31,14 @@ const setUpTable = () => {
     expectTableToBeVisible()
 }
 
-describe('unsupported types', () => {
-    beforeEach(() => {
-        cy.visit('/', EXTENDED_TIMEOUT)
-        setUpTable()
-    })
+describe('Org unit condition', () => {
     const dimensionName = TEST_DIM_ORG_UNIT
     const orgUnitName = '02 Phongsali'
 
     it('Organisation unit displays correctly', () => {
         cy.visit('/', EXTENDED_TIMEOUT)
+
+        setUpTable()
 
         selectEventProgram(ANALYTICS_PROGRAM)
 
