6ad236a8ffb437b099e39c080f73714aaea73d05
Edoardo Sabadelli <edoardo@dhis2.org>
2022-08-18 11:17:54 +0200

    chore: run Prettier

--- cypress/helpers/interpretations.js
+++ cypress/helpers/interpretations.js
@@ -1,10 +1,7 @@
 import { EXTENDED_TIMEOUT } from '../support/util.js'
 
 export const expectInterpretationsButtonToBeEnabled = () =>
-    cy
-        .getBySel('menubar')
-        .contains('Interpretations')
-        .should('be.enabled')
+    cy.getBySel('menubar').contains('Interpretations').should('be.enabled')
 
 export const expectInterpretationFormToBeVisible = () =>
     cy
