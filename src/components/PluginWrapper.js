2d64ace960ac78c1aed6ee86dbdbd05e38fa7bbf
Edoardo Sabadelli <edoardo@dhis2.org>
2022-09-13 09:33:14 +0200

    refactor: use postRobot for communication with parent

--- src/components/PluginWrapper.js
+++ src/components/PluginWrapper.js
@@ -1,30 +1,28 @@
-import React, { useEffect, useRef, useState } from 'react'
-//import { Visualization } from './Visualization/Visualization.js'
+import postRobot from '@krakenjs/post-robot'
+import React, { useEffect, useState } from 'react'
+import { Visualization } from './components/Visualization/Visualization.js'
 
 const PluginWrapper = () => {
-    const ref = useRef(null)
     const [propsFromParent, setPropsFromParent] = useState()
 
     useEffect(() => {
-        const pluginWrapper = ref.current
-
-        pluginWrapper.addEventListener('message', (event) => {
-            console.log('message received', event)
-
-            // TODO check event.source for security
-
-            setPropsFromParent(event.data)
-        })
-
-        window.top.postMessage('getProps')
+        postRobot.send(window.top, 'getProps')
+        .then((event) => setPropsFromParent(event.data))
+        .catch((err) => console.error(err))
     }, [])
 
     return (
-        <div ref={ref}>
-         {/* propsFromParent && <Visualization {...propsFromParent} /> */}
-        {'this is the LL plugin.'}
-        {`passed props are: ${Object.keys(propsFromParent)}`}
-        </div>
+        propsFromParent ? (
+            <div
+                style={{
+                    display: 'flex',
+                    height: '100%',
+                    overflow: 'hidden',
+                }}
+            >
+                <Visualization {...propsFromParent} />
+            </div>
+        ) : null
     )
 }
 
