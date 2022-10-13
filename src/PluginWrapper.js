d5a2a021e5e195b6d8bd2ea43a0983981fc83475
Kai Vandivier <kpvandivier@gmail.com>
2022-10-13 13:33:36 +0200

    feat: ability to remove cached data

--- src/PluginWrapper.js
+++ src/PluginWrapper.js
@@ -1,6 +1,7 @@
 import { useCacheableSection, CacheableSection } from '@dhis2/app-runtime'
 import { CenteredContent, CircularLoader, Layer } from '@dhis2/ui'
 import postRobot from '@krakenjs/post-robot'
+import PropTypes from 'prop-types'
 import React, { useEffect, useState } from 'react'
 import { Visualization } from './components/Visualization/Visualization.js'
 
@@ -14,17 +15,44 @@ const LoadingMask = () => {
     )
 }
 
-// eslint-disable-next-line react/prop-types
-const CacheableSectionWrapper = ({ id, children, cacheNow }) => {
-    // This doesn't work without PWA enabled -
-    // todo: add an error to the app runtime
-    const { startRecording } = useCacheableSection(id)
+const CacheableSectionWrapper = ({
+    id,
+    children,
+    cacheNow,
+    isParentCached,
+}) => {
+    const { startRecording, isCached, remove } = useCacheableSection(id)
 
-    React.useEffect(() => {
+    useEffect(() => {
         if (cacheNow) {
             startRecording({ onError: console.error })
         }
+
+        // NB: Adding `startRecording` to dependencies causes
+        // an infinite recording loop as-is (probably need to memoize it)
+        // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [cacheNow])
+
+    useEffect(() => {
+        const listener = postRobot.on(
+            'removeCachedData',
+            // todo: check domain too; differs based on deployment env though
+            { window: window.top },
+            () => remove()
+        )
+
+        return () => listener.cancel()
+    }, [remove])
+
+    useEffect(() => {
+        // Synchronize cache state on load or prop update
+        // -- a back-up to imperative `removeCachedData`
+        if (!isParentCached && isCached) {
+            remove()
+        }
+
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [isParentCached])
 
     return (
         <CacheableSection id={id} loadingMask={LoadingMask}>
@@ -32,17 +60,32 @@ const CacheableSectionWrapper = ({ id, children, cacheNow }) => {
         </CacheableSection>
     )
 }
+CacheableSectionWrapper.propTypes = {
+    cacheNow: PropTypes.bool,
+    children: PropTypes.node,
+    id: PropTypes.string,
+    isParentCached: PropTypes.bool,
+}
 
 const PluginWrapper = () => {
     const [propsFromParent, setPropsFromParent] = useState()
 
+    const receivePropsFromParent = (event) => setPropsFromParent(event.data)
+
     useEffect(() => {
         postRobot
             .send(window.top, 'getProps')
-            .then((event) => {
-                setPropsFromParent(event.data)
-            })
+            .then(receivePropsFromParent)
             .catch((err) => console.error(err))
+
+        // Allow parent to update props
+        const listener = postRobot.on(
+            'newProps',
+            { window: window.top /* Todo: check domain */ },
+            receivePropsFromParent
+        )
+
+        return () => listener.cancel()
     }, [])
 
     return propsFromParent ? (
@@ -56,6 +99,7 @@ const PluginWrapper = () => {
             <CacheableSectionWrapper
                 id={propsFromParent.cacheId}
                 cacheNow={propsFromParent.recordOnNextLoad}
+                isParentCached={propsFromParent.isParentCached}
             >
                 <Visualization {...propsFromParent} />
             </CacheableSectionWrapper>
