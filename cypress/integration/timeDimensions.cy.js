51d888392debe0e760d8a952c5d6c080fa4d29e3
Jan Henrik Øverland <janhenrik.overland@gmail.com>
2022-11-25 13:28:29 +0100

    test: tests aligned with 2.38.1

--- cypress/integration/timeDimensions.cy.js
+++ cypress/integration/timeDimensions.cy.js
@@ -55,15 +55,17 @@ const assertTimeDimension = (dimension) => {
     })
 }
 
-// TODO: enable when 2.38.2 is released
+// TODO: remove this and enable the next test for 2.38 when 2.38.2 is released
 describe(['>37', '<39'], 'time dimensions', () => {
     beforeEach(() => {
         cy.visit('/', EXTENDED_TIMEOUT)
     })
 
-    timeDimensions.forEach((dimension) => {
-        assertTimeDimension(dimension)
-    })
+    timeDimensions
+        .filter((dim) => dim.id !== DIMENSION_ID_LAST_UPDATED)
+        .forEach((dimension) => {
+            assertTimeDimension(dimension)
+        })
 })
 
 describe(['>=39'], 'time dimensions', () => {
