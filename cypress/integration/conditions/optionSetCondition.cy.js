3dbd4b24ba29a34d6ddda27113a8642991ef6dc8
Jan Henrik Øverland <janhenrik.overland@gmail.com>
2022-11-29 12:47:23 +0100

    test: optionset condition

--- cypress/integration/conditions/optionSetCondition.cy.js
+++ cypress/integration/conditions/optionSetCondition.cy.js
@@ -26,64 +26,90 @@ import { EXTENDED_TIMEOUT } from '../../support/util.js'
 // there was a bug in 38 showing the code instead of name
 // it's supposed to be fixed in 39, but the test fails due to the same issue
 
-describe(['>=40'], 'Option set condition', () => {
-    it('Option set (number) displays correctly', () => {
-        const dimensionName = 'E2E - Number (option set)'
-        const filteredOutOptionName = 'Four'
-        const filteredOptionName = 'Eight'
+const assertNumericOptionSet = ({
+    tableFilteredOptionName,
+    tableFilteredOutOptionName,
+    selectorFilteredOptionName,
+}) => {
+    const dimensionName = 'E2E - Number (option set)'
 
-        cy.visit('/', EXTENDED_TIMEOUT)
+    cy.visit('/', EXTENDED_TIMEOUT)
 
-        selectEventWithProgram(E2E_PROGRAM)
+    selectEventWithProgram(E2E_PROGRAM)
 
-        selectRelativePeriod({
-            label: E2E_PROGRAM[DIMENSION_ID_EVENT_DATE],
-            period: TEST_REL_PE_LAST_YEAR,
+    selectRelativePeriod({
+        label: E2E_PROGRAM[DIMENSION_ID_EVENT_DATE],
+        period: TEST_REL_PE_LAST_YEAR,
+    })
+
+    clickMenubarUpdateButton()
+
+    expectTableToBeVisible()
+
+    openDimension(dimensionName)
+
+    cy.getBySel('button-add-condition').should('not.exist')
+
+    cy.contains('Add to Columns').click()
+
+    clickMenubarUpdateButton()
+
+    expectTableToBeVisible()
+
+    expectTableToContainValue(tableFilteredOptionName)
+    expectTableToContainValue(tableFilteredOutOptionName)
+
+    cy.getBySelLike('layout-chip').contains(`${dimensionName}: all`)
+
+    openDimension(dimensionName)
+
+    typeInput(
+        'option-set-left-header-filter-input-field',
+        selectorFilteredOptionName
+    )
+
+    cy.getBySel('option-set-transfer-sourceoptions')
+        .contains(selectorFilteredOptionName)
+        .dblclick()
+
+    cy.getBySel('conditions-modal').contains('Update').click()
+
+    assertChipContainsText(`${dimensionName}: 1 selected`)
+
+    assertTooltipContainsEntries([selectorFilteredOptionName])
+
+    expectTableToNotContainValue(tableFilteredOutOptionName)
+    expectTableToContainValue(tableFilteredOptionName)
+
+    expectTableToMatchRows([
+        `${getPreviousYearStr()}-12-23`,
+        `${getPreviousYearStr()}-12-22`,
+    ])
+}
+
+describe('Option set condition', () => {
+    it(['>37', '<39'], 'Option set (number) displays correctly (2.38)', () => {
+        assertNumericOptionSet({
+            tableFilteredOptionName: 'Ei ght',
+            tableFilteredOutOptionName: 'F our',
+            selectorFilteredOptionName: 'Eight',
         })
+    })
 
-        clickMenubarUpdateButton()
+    it(['>38', '<40'], 'Option set (number) displays correctly (2.39)', () => {
+        assertNumericOptionSet({
+            tableFilteredOptionName: '8',
+            tableFilteredOutOptionName: '4',
+            selectorFilteredOptionName: 'Eight',
+        })
+    })
 
-        expectTableToBeVisible()
-
-        openDimension(dimensionName)
-
-        cy.getBySel('button-add-condition').should('not.exist')
-
-        cy.contains('Add to Columns').click()
-
-        clickMenubarUpdateButton()
-
-        expectTableToBeVisible()
-
-        expectTableToContainValue(filteredOutOptionName)
-        expectTableToContainValue(filteredOptionName)
-
-        cy.getBySelLike('layout-chip').contains(`${dimensionName}: all`)
-
-        openDimension(dimensionName)
-
-        typeInput(
-            'option-set-left-header-filter-input-field',
-            filteredOptionName
-        )
-
-        cy.getBySel('option-set-transfer-sourceoptions')
-            .contains(filteredOptionName)
-            .dblclick()
-
-        cy.getBySel('conditions-modal').contains('Update').click()
-
-        assertChipContainsText(`${dimensionName}: 1 selected`)
-
-        assertTooltipContainsEntries([filteredOptionName])
-
-        expectTableToNotContainValue(filteredOutOptionName)
-        expectTableToContainValue(filteredOptionName)
-
-        expectTableToMatchRows([
-            `${getPreviousYearStr()}-12-23`,
-            `${getPreviousYearStr()}-12-22`,
-        ])
+    it(['>=40'], 'Option set (number) displays correctly (2.40+)', () => {
+        assertNumericOptionSet({
+            tableFilteredOptionName: 'Ei ght',
+            tableFilteredOutOptionName: 'F our',
+            selectorFilteredOptionName: 'Eight',
+        })
     })
 
     it('Option set (text) displays correctly', () => {
