fd6546a32e3ee9ce22546b3c081dbeb69d29cd99
Edoardo Sabadelli <edoardo@dhis2.org>
2022-12-06 15:30:31 +0100

    fix: set default for displayProperty
    The prop is required as it is used and passed around, but if is not
    passed from the consumer the plugin should still work.

--- src/components/Visualization/Visualization.js
+++ src/components/Visualization/Visualization.js
@@ -371,6 +371,7 @@ export const Visualization = ({
 }
 
 Visualization.defaultProps = {
+    displayProperty: 'name',
     isVisualizationLoading: false,
     onResponsesReceived: Function.prototype,
 }
