6b9ef30197d6ba2cac607ce272d667d49d0f74df
Jen Jones Arnesen <jennifer@dhis2.org>
2022-12-12 14:02:21 +0100

    fix: cypress - wait for options request to complete

--- cypress/helpers/transfer.js
+++ cypress/helpers/transfer.js
@@ -2,7 +2,12 @@ import { EXTENDED_TIMEOUT } from '../support/util.js'
 import { typeInput } from './common.js'
 
 export const searchAndSelectInTransfer = (name) => {
+    const searchRegex = new RegExp(encodeURI(name))
+    cy.intercept('GET', searchRegex).as(
+        'fetchOptions'
+    )
     typeInput('option-set-left-header-filter-input-field', name)
+    cy.wait('@fetchOptions').its('response.statusCode').should('eq', 200)
 
     cy.getBySel('option-set-left-header-filter-input-field')
         .find('input')
