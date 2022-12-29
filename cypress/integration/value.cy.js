b3ea575c0eab756ed375b763d5c9dff0a37cb75c
Jan Henrik Øverland <janhenrik.overland@gmail.com>
2022-12-29 14:16:38 +0100

    test: update white-space test

--- cypress/integration/value.cy.js
+++ cypress/integration/value.cy.js
@@ -20,31 +20,39 @@ import {
 import { selectEventWithProgramDimensions } from '../helpers/dimensions.js'
 import { clickMenubarUpdateButton } from '../helpers/menubar.js'
 import { selectRelativePeriod } from '../helpers/period.js'
+import { goToStartPage } from '../helpers/startScreen.js'
 import { getTableDataCells } from '../helpers/table.js'
-import { EXTENDED_TIMEOUT } from '../support/util.js'
 
 describe('value', () => {
-    beforeEach(() => {
-        cy.visit('/', EXTENDED_TIMEOUT)
+    it('has the correct white-space css', () => {
+        goToStartPage()
+
+        const programDimensionsWithWrap = [
+            TEST_DIM_TEXT,
+            TEST_DIM_TEXT_OPTIONSET,
+            TEST_DIM_NUMBER_OPTIONSET,
+        ]
+        const programDimensionsWithoutWrap = [
+            TEST_DIM_NUMBER,
+            TEST_DIM_INTEGER,
+            TEST_DIM_INTEGER_POSITIVE,
+            TEST_DIM_INTEGER_NEGATIVE,
+            TEST_DIM_INTEGER_ZERO_OR_POSITIVE,
+            TEST_DIM_PERCENTAGE,
+            TEST_DIM_UNIT_INTERVAL,
+            TEST_DIM_TIME,
+            TEST_DIM_DATE,
+            TEST_DIM_DATETIME,
+            TEST_DIM_PHONE_NUMBER,
+        ]
+        const programDimensions = [
+            ...programDimensionsWithWrap,
+            ...programDimensionsWithoutWrap,
+        ]
 
         selectEventWithProgramDimensions({
             programName: E2E_PROGRAM.programName,
-            dimensions: [
-                TEST_DIM_TEXT,
-                TEST_DIM_TEXT_OPTIONSET,
-                TEST_DIM_NUMBER_OPTIONSET,
-                TEST_DIM_NUMBER,
-                TEST_DIM_INTEGER,
-                TEST_DIM_INTEGER_POSITIVE,
-                TEST_DIM_INTEGER_NEGATIVE,
-                TEST_DIM_INTEGER_ZERO_OR_POSITIVE,
-                TEST_DIM_PERCENTAGE,
-                TEST_DIM_UNIT_INTERVAL,
-                TEST_DIM_TIME,
-                TEST_DIM_DATE,
-                TEST_DIM_DATETIME,
-                TEST_DIM_PHONE_NUMBER,
-            ],
+            dimensions: programDimensions,
         })
 
         selectRelativePeriod({
@@ -53,18 +61,24 @@ describe('value', () => {
         })
 
         clickMenubarUpdateButton()
-    })
 
-    it('has the correct white-space css', () => {
         const shouldHaveWhiteSpace = (index, value) =>
             getTableDataCells()
                 .eq(index)
                 .invoke('css', 'white-space')
                 .should('equal', value)
 
-        ;[0, 1, 2, 3].forEach((index) => shouldHaveWhiteSpace(index, 'normal'))
-        ;[4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15].forEach((index) =>
-            shouldHaveWhiteSpace(index, 'nowrap')
+        const dimensions = [
+            'Organisation unit name',
+            ...programDimensions,
+            E2E_PROGRAM[DIMENSION_ID_EVENT_DATE],
+        ]
+
+        programDimensionsWithWrap.forEach((dim) =>
+            shouldHaveWhiteSpace(dimensions.indexOf(dim), 'normal')
+        )
+        programDimensionsWithoutWrap.forEach((dim) =>
+            shouldHaveWhiteSpace(dimensions.indexOf(dim), 'nowrap')
         )
     })
 })
