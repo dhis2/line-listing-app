6b8eacabacabf6b987a9920bc7592c7742c96378
martinkrulltott <martin@moid.se>
2022-12-30 11:32:11 +0100

    test: split date check into separate parts

--- cypress/integration/save.cy.js
+++ cypress/integration/save.cy.js
@@ -55,7 +55,9 @@ describe('save', () => {
     it('new AO without name saves correctly', () => {
         cy.clock(cy.clock(Date.UTC(2022, 11, 29), ['Date'])) // month is 0-indexed, 11 = December
         const EXPECTED_AO_NAME_PART_1 = 'Untitled Line list visualization'
-        const EXPECTED_AO_NAME_PART_2 = '29 Dec' // locally the date is "29 Dec 2022" but on CI it's "29 Dec, 2022" with a comma, so it's split into two parts to cater for both cases
+        const EXPECTED_AO_NAME_PART_2 = '29'
+        const EXPECTED_AO_NAME_PART_3 = 'Dec'
+        const EXPECTED_AO_NAME_PART_4 = '2022' // locally the date is "29 Dec 2022" but on CI it's "Dec 29, 2022", so it's split into different parts to cater for both cases
         const UPDATED_AO_NAME = `TEST ${new Date().toLocaleString()}`
         setupTable()
 
@@ -63,12 +65,16 @@ describe('save', () => {
         saveVisualization()
         expectAOTitleToContain(EXPECTED_AO_NAME_PART_1)
         expectAOTitleToContain(EXPECTED_AO_NAME_PART_2)
+        expectAOTitleToContain(EXPECTED_AO_NAME_PART_3)
+        expectAOTitleToContain(EXPECTED_AO_NAME_PART_4)
         expectTableToBeVisible()
 
         // save as without name change
         saveVisualizationAs()
         expectAOTitleToContain(EXPECTED_AO_NAME_PART_1)
         expectAOTitleToContain(EXPECTED_AO_NAME_PART_2)
+        expectAOTitleToContain(EXPECTED_AO_NAME_PART_3)
+        expectAOTitleToContain(EXPECTED_AO_NAME_PART_4)
         expectAOTitleToContain('(copy)')
         expectTableToBeVisible()
 
