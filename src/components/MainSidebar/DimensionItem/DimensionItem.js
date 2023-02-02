1bd097aeb4a4fd02bff2c5b3273c97b55ab36f02
Jan Henrik Øverland <janhenrik.overland@gmail.com>
2023-02-02 15:14:52 +0100

    fix: use custom icon by joe

--- src/components/MainSidebar/DimensionItem/DimensionItem.js
+++ src/components/MainSidebar/DimensionItem/DimensionItem.js
@@ -1,4 +1,4 @@
-import { IconAdd16, IconCross16, Tooltip } from '@dhis2/ui'
+import { IconAdd16 } from '@dhis2/ui'
 import { useSortable } from '@dnd-kit/sortable'
 import { CSS } from '@dnd-kit/utilities'
 import cx from 'classnames'
@@ -13,6 +13,24 @@ import {
 import IconButton from '../../IconButton/IconButton.js'
 import styles from './DimensionItem.module.css'
 import { DimensionItemBase } from './DimensionItemBase.js'
+
+// Joe prefers this icon as the one in ui is too light
+const IconCross16 = () => (
+    <svg
+        width="16"
+        height="16"
+        viewBox="0 0 16 16"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+    >
+        <path
+            fillRule="evenodd"
+            clipRule="evenodd"
+            d="M4.70711 3.29289C4.31658 2.90237 3.68342 2.90237 3.29289 3.29289C2.90237 3.68342 2.90237 4.31658 3.29289 4.70711L6.58579 8L3.29289 11.2929C2.90237 11.6834 2.90237 12.3166 3.29289 12.7071C3.68342 13.0976 4.31658 13.0976 4.70711 12.7071L8 9.41421L11.2929 12.7071C11.6834 13.0976 12.3166 13.0976 12.7071 12.7071C13.0976 12.3166 13.0976 11.6834 12.7071 11.2929L9.41421 8L12.7071 4.70711C13.0976 4.31658 13.0976 3.68342 12.7071 3.29289C12.3166 2.90237 11.6834 2.90237 11.2929 3.29289L8 6.58579L4.70711 3.29289Z"
+            fill="#4A5768"
+        />
+    </svg>
+)
 
 export const DimensionItem = ({
     id,
@@ -79,13 +97,14 @@ export const DimensionItem = ({
                 dataTest={`dimension-item-${id}`}
                 contextMenu={
                     !disabled && (
-                        // <DimensionMenu
-                        //     dimensionId={id}
-                        //     dimensionMetadata={dimensionMetadata}
-                        // />
-
                         <ActionButton
-                            icon={selected ? <IconCross16 /> : <IconAdd16 />}
+                            icon={
+                                selected ? (
+                                    <IconCross16 />
+                                ) : (
+                                    <IconAdd16 color="var(--colors-grey600)" />
+                                )
+                            }
                             onClick={(e) => {
                                 e && e.stopPropagation()
 
@@ -111,9 +130,7 @@ export const DimensionItem = ({
 const ActionButton = ({ onClick, icon }) => {
     return (
         <div className={cx(styles.hidden)}>
-            <Tooltip content={'Add to Columns'} placement="top" openDelay={500}>
-                <IconButton onClick={onClick}>{icon}</IconButton>
-            </Tooltip>
+            <IconButton onClick={onClick}>{icon}</IconButton>
         </div>
     )
 }
