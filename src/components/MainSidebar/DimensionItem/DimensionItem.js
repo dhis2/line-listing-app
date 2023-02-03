bbc2e9787e2d6fb84dc0b172d848335c6287f14e
Jan Henrik Øverland <janhenrik.overland@gmail.com>
2023-02-03 09:52:46 +0100

    fix: change remove icon

--- src/components/MainSidebar/DimensionItem/DimensionItem.js
+++ src/components/MainSidebar/DimensionItem/DimensionItem.js
@@ -13,19 +13,16 @@ import { DimensionActionButton } from './DimensionActionButton.js'
 import { DimensionItemBase } from './DimensionItemBase.js'
 
 // Joe prefers this icon as the one in ui is too light
-const IconCross16 = () => (
+const IconSubtract16 = () => (
     <svg
         width="16"
         height="16"
         viewBox="0 0 16 16"
-        fill="none"
         xmlns="http://www.w3.org/2000/svg"
     >
         <path
-            fillRule="evenodd"
-            clipRule="evenodd"
-            d="M4.70711 3.29289C4.31658 2.90237 3.68342 2.90237 3.29289 3.29289C2.90237 3.68342 2.90237 4.31658 3.29289 4.70711L6.58579 8L3.29289 11.2929C2.90237 11.6834 2.90237 12.3166 3.29289 12.7071C3.68342 13.0976 4.31658 13.0976 4.70711 12.7071L8 9.41421L11.2929 12.7071C11.6834 13.0976 12.3166 13.0976 12.7071 12.7071C13.0976 12.3166 13.0976 11.6834 12.7071 11.2929L9.41421 8L12.7071 4.70711C13.0976 4.31658 13.0976 3.68342 12.7071 3.29289C12.3166 2.90237 11.6834 2.90237 11.2929 3.29289L8 6.58579L4.70711 3.29289Z"
-            fill="#4A5768"
+            fill="var(--colors-grey600)"
+            d="M2 8C2 7.44772 2.44772 7 3 7H13C13.5523 7 14 7.44772 14 8V8C14 8.55228 13.5523 9 13 9H3C2.44772 9 2 8.55228 2 8V8Z"
         />
     </svg>
 )
@@ -98,7 +95,7 @@ export const DimensionItem = ({
                         <DimensionActionButton
                             icon={
                                 selected ? (
-                                    <IconCross16 />
+                                    <IconSubtract16 />
                                 ) : (
                                     <IconAdd16 color="var(--colors-grey600)" />
                                 )
