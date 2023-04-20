e91e791afe21a0d8c7036d7fb7993cba54e37339
HendrikThePendric <hendrik@dhis2.org>
2023-04-20 16:05:36 +0200

    chore: update general cypress config

---
+++ cypress.config.js
@@ -0,0 +1,35 @@
+const { chromeAllowXSiteCookies } = require('@dhis2/cypress-plugins')
+const { defineConfig } = require('cypress')
+const {
+    excludeByVersionTags,
+} = require('./cypress/plugins/excludeByVersionTags.js')
+
+// This function will be modified when we add plugins
+async function setupNodeEvents(on, config) {
+    chromeAllowXSiteCookies(on, config)
+    excludeByVersionTags(on, config)
+
+    if (!config.env.dhis2InstanceVersion) {
+        throw new Error(
+            'dhis2InstanceVersion is missing. Check the README for more information.'
+        )
+    }
+
+    return config
+}
+
+module.exports = defineConfig({
+    projectId: 'm5qvjx',
+    e2e: {
+        setupNodeEvents,
+        baseUrl: 'http://localhost:3000',
+        specPattern: 'cypress/integration/**/*.cy.js',
+        viewportWidth: 1280,
+        viewportHeight: 800,
+    },
+    env: {
+        dhis2DatatestPrefix: 'dhis2-linelisting',
+        networkMode: 'live',
+        dhis2ApiVersion: '41',
+    },
+})
