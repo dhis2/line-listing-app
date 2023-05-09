41ed4b250aaa963a53ce295472c5316f205c13c6
HendrikThePendric <hendrik@dhis2.org>
2023-05-09 15:37:55 +0200

    chore: allow styled-jsx in project

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,4 +2,8 @@ const { config } = require('@dhis2/cli-style')
 
 module.exports = {
     extends: [config.eslintReact, 'plugin:cypress/recommended'],
+    rules: {
+        // TODO: remove after extracting components
+        'react/no-unknown-property': ['error', { ignore: ['jsx'] }],
+    },
 }
