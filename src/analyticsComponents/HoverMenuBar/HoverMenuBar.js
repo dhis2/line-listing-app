705c48c93382d5955ed366d14a162777bfe33d95
HendrikThePendric <hendrik@dhis2.org>
2023-05-15 11:05:20 +0200

    feat: port file-menu to new hoverable menubar

--- src/analyticsComponents/HoverMenuBar/HoverMenuBar.js
+++ src/analyticsComponents/HoverMenuBar/HoverMenuBar.js
@@ -1,14 +1,28 @@
 import PropTypes from 'prop-types'
-import React from 'react'
+import React, { useCallback } from 'react'
 import { HoverMenuBarContext } from './HoverMenuBarContext.js'
 import { useHoverMenuBarState } from './useHoverMenuBarState.js'
 
 export const HoverMenuBar = ({ children }) => {
     const hoverMenuBarState = useHoverMenuBarState()
+    const { closeMenu } = hoverMenuBarState
+    const closeMenuWithEsc = useCallback(
+        (event) => {
+            if (event.keyCode === 27) {
+                closeMenu()
+            }
+        },
+        [closeMenu]
+    )
 
     return (
         <HoverMenuBarContext.Provider value={hoverMenuBarState}>
-            {children}
+            <div onKeyDown={closeMenuWithEsc}>
+                {children}
+                <style jsx>{`
+                    display: flex;
+                `}</style>
+            </div>
         </HoverMenuBarContext.Provider>
     )
 }
