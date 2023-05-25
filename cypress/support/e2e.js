3b76fbeb54c847781de1999d979452e689241d8a
HendrikThePendric <hendrik@dhis2.org>
2023-05-25 14:31:53 +0200

    chore: remove unused deps and cleanup support e2e

--- cypress/support/e2e.js
+++ cypress/support/e2e.js
@@ -1,10 +1,4 @@
-import '@this-dot/cypress-indexeddb'
-// import { updateBaseUrlIndexedDb } from './updateBaseUrlIndexedDb.js'
 import './commands.js'
-
-// const appName = 'Line Listing'
-// const baseUrlDbName = 'dhis2-base-url-db'
-// const baseUrlStoreName = 'dhis2-base-url-store'
 
 /**
  * Custom login command, can be used to login or switch between sessions.
@@ -30,22 +24,9 @@ Cypress.Commands.add('login', (user) => {
 
             // Set base url for the app platform
             window.localStorage.setItem('DHIS2_BASE_URL', user.server)
-            // Update indexDB to the correct baseUrl value if needed
-            // updateBaseUrlIndexedDb({
-            //     appName,
-            //     baseUrlDbName,
-            //     baseUrlStoreName,
-            //     baseUrl: user.server,
-            // })
         },
         {
             validate: () => {
-                // Check indexedDB is updated correctly
-                // cy.openIndexedDb(baseUrlDbName)
-                //     .createObjectStore(baseUrlStoreName)
-                //     .readItem('Line Listing')
-                //     .should('deep.equal', { appName, baseUrl: user.server })
-
                 // Check API is returning the expected response
                 cy.request(`${user.server}/api/me`).should((response) => {
                     expect(response.status).to.eq(200)
