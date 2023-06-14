e0b5886c91d9293a035d39952b33e321c64ea5c2
HendrikThePendric <hendrik@dhis2.org>
2023-06-14 08:46:23 +0200

    chore: decrease numTestsKeptInMemory to prevent `cypress open` crashing

--- cypress.config.js
+++ cypress.config.js
@@ -34,7 +34,7 @@ module.exports = defineConfig({
         // Enabled to reduce the risk of out-of-memory issues
         experimentalMemoryManagement: true,
         // Set to a low number to reduce the risk of out-of-memory issues
-        numTestsKeptInMemory: 20,
+        numTestsKeptInMemory: 5,
         /* When allowing 2 retries on CI, the test suite will pass if
          * it's flaky. And/but we also get to identify flaky tests on the
          * Cypress Dashboard. */
