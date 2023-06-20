2a0891ac2bb9a6327a9a48880176f0746bea2ff5
HendrikThePendric <hendrik@dhis2.org>
2023-06-20 15:31:37 +0200

    chore: remove lint rule needed while building analytics components

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,8 +2,4 @@ const { config } = require('@dhis2/cli-style')
 
 module.exports = {
     extends: [config.eslintReact, 'plugin:cypress/recommended'],
-    rules: {
-        // TODO: remove after extracting components
-        'react/no-unknown-property': ['error', { ignore: ['jsx'] }],
-    },
 }
