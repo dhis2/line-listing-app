0280f9ba139da8c054edf5c3f61fafbf52ac42fc
HendrikThePendric <hendrik@dhis2.org>
2023-10-04 10:43:29 +0200

    chore: fix translations e2e test

--- cypress/integration/translations.cy.js
+++ cypress/integration/translations.cy.js
@@ -6,39 +6,35 @@ const UPDATE_BUTTON_TRANSLATED = 'Oppdater'
 const WELCOME_MSG_ORIGINAL = 'Getting started'
 const WELCOME_MSG_TRANSLATED = 'Komme i gang'
 
-const interceptLanguage = () => {
-    cy.intercept('**userSettings**', (req) => {
-        req.reply((res) => {
-            res.send({
-                body: {
-                    ...res.body,
-                    keyUiLocale: 'nb',
-                },
-            })
+const setUserLocale = (locale) => {
+    const dhis2BaseUrl = Cypress.env('dhis2BaseUrl')
+    const dhis2InstanceVersion = Cypress.env('dhis2InstanceVersion')
+    const versionParts = dhis2InstanceVersion.split('.')
+    const apiVersion =
+        versionParts.length === 1 ? versionParts[0] : versionParts[1]
+    const url = `${dhis2BaseUrl}/api/${apiVersion}/userSettings/keyUiLocale`
+
+    if (locale) {
+        cy.request({
+            url,
+            method: 'POST',
+            body: 'nb',
         })
-    })
-    cy.intercept(
-        '**me?fields=authorities,avatar,email,name,settings**',
-        (req) => {
-            req.reply((res) => {
-                res.send({
-                    body: {
-                        ...res.body,
-                        settings: {
-                            ...res.body.settings,
-                            keyUiLocale: 'nb',
-                        },
-                    },
-                })
-            })
-        }
-    )
+            .its('status')
+            .should('equal', 200)
+    } else {
+        cy.request({
+            url,
+            method: 'DELETE',
+        })
+            .its('status')
+            .should('equal', 200)
+    }
 }
 
 describe('Translations', () => {
     it('translated language display correctly in the app', () => {
-        interceptLanguage()
-
+        setUserLocale('nb')
         goToStartPage(true)
 
         cy.contains(WELCOME_MSG_TRANSLATED, EXTENDED_TIMEOUT).should(
@@ -46,10 +42,10 @@ describe('Translations', () => {
         )
 
         cy.contains(WELCOME_MSG_ORIGINAL).should('not.exist')
+        setUserLocale(null)
     })
     it('translated language display correctly in an Analytics component', () => {
-        interceptLanguage()
-
+        setUserLocale('nb')
         goToStartPage(true)
 
         cy.contains(UPDATE_BUTTON_TRANSLATED, EXTENDED_TIMEOUT).should(
@@ -57,8 +53,10 @@ describe('Translations', () => {
         )
 
         cy.contains(UPDATE_BUTTON_ORIGINAL).should('not.exist')
+        setUserLocale(null)
     })
     it('original language display correctly in the app', () => {
+        setUserLocale(null)
         goToStartPage(true)
 
         cy.contains(WELCOME_MSG_ORIGINAL, EXTENDED_TIMEOUT).should('be.visible')
@@ -66,6 +64,7 @@ describe('Translations', () => {
         cy.contains(WELCOME_MSG_TRANSLATED).should('not.exist')
     })
     it('original language display correctly in an Analytics component', () => {
+        setUserLocale(null)
         goToStartPage(true)
 
         cy.contains(UPDATE_BUTTON_ORIGINAL, EXTENDED_TIMEOUT).should(
