881a9bbe9cb3a30d6926b8dadb4cc1c0bfe0aac2
HendrikThePendric <hendrik@dhis2.org>
2023-10-05 16:50:23 +0200

    chore: add assertions to debug unauthorised issue on CI

--- cypress/helpers/startScreen.js
+++ cypress/helpers/startScreen.js
@@ -3,6 +3,19 @@ import { EXTENDED_TIMEOUT } from '../support/util.js'
 export const goToStartPage = (skipEval) => {
     cy.visit('/', EXTENDED_TIMEOUT).log(Cypress.env('dhis2BaseUrl'))
 
+    cy.intercept('GET', '**/system/info**', (req) => {
+        expect(req.url).to.equal('http://localhost:8080/api/system/info')
+    }).as('systemInfoRequest')
+
+    cy.wait('@systemInfoRequest').its('response.statusCode').should('eq', 200)
+
+    const cypressEnv = Cypress.env()
+
+    expect(cypressEnv.dhis2BaseUrl).to.eq('http://localhost:8080')
+    expect(cypressEnv.dhis2InstanceVersion).be.oneOf(['2.40.1', '2.38.4.3'])
+    expect(cypressEnv.dhis2Password).to.eq('district')
+    expect(cypressEnv.dhis2Username).to.eq('admin')
+
     if (!skipEval) {
         expectStartScreenToBeVisible()
     }
