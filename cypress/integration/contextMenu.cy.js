4aba8e5873c48af2654ea7a4ad3be253153a6a13
HendrikThePendric <hendrik@dhis2.org>
2023-10-05 17:48:41 +0200

    chore: move debug output to it blocks so hopefully we see output

--- cypress/integration/contextMenu.cy.js
+++ cypress/integration/contextMenu.cy.js
@@ -16,6 +16,26 @@ describe('using the layout chip context menu', () => {
     it('moves item', () => {
         goToStartPage()
 
+        cy.intercept('GET', '**/system/info**', (req) => {
+            expect(req.url).to.equal('http://localhost:8080/api/system/info')
+        }).as('systemInfoRequest')
+
+        cy.wait('@systemInfoRequest')
+            .its('response.statusCode')
+            .should('eq', 200)
+
+        const cypressEnv = Cypress.env()
+
+        expect(cypressEnv.dhis2BaseUrl).to.eq('http://localhost:8080')
+        expect(cypressEnv.dhis2InstanceVersion).be.oneOf(['2.40.1', '2.38.4.3'])
+        expect(cypressEnv.dhis2Password).to.eq('district')
+        expect(cypressEnv.dhis2Username).to.eq('admin')
+
+        cy.log(`dhis2BaseUrl: ${cypressEnv.dhis2BaseUrl}`)
+        cy.log(`dhis2InstanceVersion: ${cypressEnv.dhis2InstanceVersion}`)
+        cy.log(`dhis2Password: ${cypressEnv.dhis2Password}`)
+        cy.log(`dhis2Username: ${cypressEnv.dhis2Username}`)
+
         expectAxisToHaveDimension(AXIS_ID_COLUMNS, TEST_DIM_ID)
         expectAxisToNotHaveDimension(AXIS_ID_FILTERS, TEST_DIM_ID)
 
