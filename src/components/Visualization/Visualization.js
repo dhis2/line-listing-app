9445d6a5481971a2a18a929c35f59b09f29a7924
HendrikThePendric <hendrik@dhis2.org>
2023-10-12 15:26:28 +0200

    fix: take legend-key into account when computing pagination max-width (DHIS2-15465)

--- src/components/Visualization/Visualization.js
+++ src/components/Visualization/Visualization.js
@@ -149,6 +149,7 @@ export const Visualization = ({
     const shouldShowTimeDimensionWarning = isInModal && !hasTimeDimension
 
     const visualizationRef = useRef(visualization)
+    const legendKeyRef = useRef(null)
 
     const containerCallbackRef = useCallback((node) => {
         if (node === null) {
@@ -162,8 +163,12 @@ export const Visualization = ({
             const containerInnerWidth = node.clientWidth
             const scrollBox = node.querySelector('.tablescrollbox')
             const scrollbarWidth = scrollBox.offsetWidth - scrollBox.clientWidth
+            const legendKeyWidth =
+                legendKeyRef.current?.offsetWidth > 0
+                    ? legendKeyRef.current.offsetWidth + 4
+                    : 0
             const paginationMaxWidth = Math.max(
-                containerInnerWidth - scrollbarWidth,
+                containerInnerWidth - scrollbarWidth - legendKeyWidth,
                 PAGINATION_MIN_WIDTH
             )
 
@@ -355,6 +360,7 @@ export const Visualization = ({
         <div
             className={styles.legendKeyScrollbox}
             data-test="visualization-legend-key"
+            ref={legendKeyRef}
         >
             <LegendKey legendSets={uniqueLegendSets} />
         </div>
