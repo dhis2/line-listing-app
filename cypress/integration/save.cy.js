a129840a4f821511d42a2de584f0757744f74b16
martinkrulltott <martin@moid.se>
2023-11-30 11:41:22 +0100

    test: assert original name before reloading

--- cypress/integration/save.cy.js
+++ cypress/integration/save.cy.js
@@ -59,6 +59,8 @@ describe('rename', () => {
         // rename the AO, changing name only
         renameVisualization(UPDATED_AO_NAME)
 
+        expectAOTitleToContain(AO_NAME)
+
         cy.reload()
 
         expectAOTitleToContain(UPDATED_AO_NAME)
