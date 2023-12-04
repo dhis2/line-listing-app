332d1454fd16e80371ab201f6403e7bd24e5d048
Adel Bensaad <adel@dhis2.org>
2023-12-04 22:42:27 +0000

    test: enhance smoke tests with detailed descriptions and attributes

--- cypress/integration/smoke.cy.js
+++ cypress/integration/smoke.cy.js
@@ -5,12 +5,28 @@ import { EXTENDED_TIMEOUT } from '../support/util.js'
 
 describe('Smoke Test', () => {
     it('loads', () => {
+        cy.setTestDescription(
+            'Verifies that the main page of the application loads correctly and displays the expected content.'
+        )
+        cy.addTestAttributes([
+            { key: 'type', value: 'SmokeTest' },
+            { key: 'action', value: 'LoadMainPage' },
+        ])
+
         goToStartPage()
         cy.contains('Getting started', EXTENDED_TIMEOUT).should('be.visible')
         cy.title().should('equal', 'Line Listing | DHIS2')
     })
 
     it('loads with visualization id', () => {
+        cy.setTestDescription(
+            'Ensures that the application correctly loads a specific visualization identified by its ID.'
+        )
+        cy.addTestAttributes([
+            { key: 'type', value: 'SmokeTest' },
+            { key: 'action', value: 'LoadVisualizationById' },
+        ])
+
         goToAO(TEST_AO.id)
 
         cy.getBySel('titlebar', EXTENDED_TIMEOUT)
