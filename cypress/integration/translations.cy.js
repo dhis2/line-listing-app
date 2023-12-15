89c7b728e5645fb283ff1cdf420e97cea86865c3
Adel Bensaad <adel@dhis2.org>
2023-12-15 17:00:19 +0100

    test: enhance descriptions and attributes for localization tests

--- cypress/integration/translations.cy.js
+++ cypress/integration/translations.cy.js
@@ -37,6 +37,14 @@ const interceptLanguage = () => {
 
 describe('Translations', () => {
     it('translated language display correctly in the app', () => {
+        cy.setTestDescription(
+            'Checks if the application correctly displays translated text for the main interface in the Norwegian language.'
+        )
+        cy.addTestAttributes([
+            { key: 'feature', value: 'Localization' },
+            { key: 'component', value: 'MainInterface' },
+            { key: 'language', value: 'Norwegian' },
+        ])
         interceptLanguage()
 
         goToStartPage(true)
@@ -48,6 +56,14 @@ describe('Translations', () => {
         cy.contains(WELCOME_MSG_ORIGINAL).should('not.exist')
     })
     it('translated language display correctly in an Analytics component', () => {
+        cy.setTestDescription(
+            'Verifies that Analytics components render properly with translated text in Norwegian.'
+        )
+        cy.addTestAttributes([
+            { key: 'feature', value: 'Localization' },
+            { key: 'component', value: 'Analytics' },
+            { key: 'language', value: 'Norwegian' },
+        ])
         interceptLanguage()
 
         goToStartPage(true)
@@ -59,6 +75,14 @@ describe('Translations', () => {
         cy.contains(UPDATE_BUTTON_ORIGINAL).should('not.exist')
     })
     it('original language display correctly in the app', () => {
+        cy.setTestDescription(
+            'Ensures that the main interface of the application displays the original English text correctly.'
+        )
+        cy.addTestAttributes([
+            { key: 'feature', value: 'Localization' },
+            { key: 'component', value: 'MainInterface' },
+            { key: 'language', value: 'English' },
+        ])
         goToStartPage(true)
 
         cy.contains(WELCOME_MSG_ORIGINAL, EXTENDED_TIMEOUT).should('be.visible')
@@ -66,6 +90,14 @@ describe('Translations', () => {
         cy.contains(WELCOME_MSG_TRANSLATED).should('not.exist')
     })
     it('original language display correctly in an Analytics component', () => {
+        cy.setTestDescription(
+            'Confirms that Analytics components are correctly rendered with original English text.'
+        )
+        cy.addTestAttributes([
+            { key: 'feature', value: 'Localization' },
+            { key: 'component', value: 'Analytics' },
+            { key: 'language', value: 'English' },
+        ])
         goToStartPage(true)
 
         cy.contains(UPDATE_BUTTON_ORIGINAL, EXTENDED_TIMEOUT).should(
