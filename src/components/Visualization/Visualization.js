534ab55c035776a6b40762816963b382895a12c6
HendrikThePendric <hendrik@dhis2.org>
2024-01-29 10:49:57 +0100

    chore: fix formatting

--- src/components/Visualization/Visualization.js
+++ src/components/Visualization/Visualization.js
@@ -506,7 +506,10 @@ export const Visualization = ({
                         )}
                         scrollWidth="100%"
                         width="auto"
-                        className={cx(styles.dataTable, 'push-analytics-linelist-table')}
+                        className={cx(
+                            styles.dataTable,
+                            'push-analytics-linelist-table'
+                        )}
                         dataTest="line-list-table"
                         ref={dataTableRef}
                     >
