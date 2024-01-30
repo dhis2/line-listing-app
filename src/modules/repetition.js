81644fe904f9b4038030dc4af8e54f16b6dd2f19
martinkrulltott <martin@moid.se>
2024-01-30 15:36:47 +0000

    fix: use correct dimension id format for repetitions

--- src/modules/repetition.js
+++ src/modules/repetition.js
@@ -1,5 +1,6 @@
 import { layoutGetAllDimensions } from '@dhis2/analytics'
 import { parseCurrentRepetition } from './ui.js'
+import { formatDimensionId } from './utils.js'
 
 export const getRepetitionFromVisualisation = (vis) => {
     const repetitionByDimension = {}
@@ -7,9 +8,12 @@ export const getRepetitionFromVisualisation = (vis) => {
         .filter((d) => d.repetition)
         .forEach((d) => {
             repetitionByDimension[
-                d.programStage?.id
-                    ? `${d.programStage.id}.${d.dimension}`
-                    : d.dimension
+                formatDimensionId({
+                    dimensionId: d.dimension,
+                    programStageId: d.programStage?.id,
+                    programId: d.program?.id,
+                    outputType: vis.outputType,
+                })
             ] = parseCurrentRepetition(d.repetition.indexes)
         })
     return repetitionByDimension
