4457c8834898804ec0ed6daf5425994d06a445ff
Martin Ohlson <martin@moid.se>
2024-02-26 11:14:26 +0000

    test: expect more TET dimensions for new db

--- cypress/integration/createTrackedEntity.cy.js
+++ cypress/integration/createTrackedEntity.cy.js
@@ -51,7 +51,7 @@ describe(['>=41'], 'tracked entity', () => {
         cy.getBySel('main-sidebar').contains('Person dimensions').click()
         cy.getBySel('tracked-entity-dimensions-list')
             .children()
-            .should('have.length', 33)
+            .should('have.length', 50)
         clickAddRemoveTrackedEntityTypeDimensions(entityDimensionName)
 
         // select a program and add program dimensions
