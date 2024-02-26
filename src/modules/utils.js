0f2337268efec39c5df10c628fbcbc724192943d
Martin Ohlson <martin@moid.se>
2024-02-26 15:16:09 +0000

    refactor: reverse the order of getDimensionsWithSuffix

--- src/modules/utils.js
+++ src/modules/utils.js
@@ -94,62 +94,58 @@ export const getDimensionsWithSuffix = ({
     })
 
     if (
-        [OUTPUT_TYPE_ENROLLMENT, OUTPUT_TYPE_TRACKED_ENTITY].includes(inputType)
+        ![OUTPUT_TYPE_ENROLLMENT, OUTPUT_TYPE_TRACKED_ENTITY].includes(
+            inputType
+        )
     ) {
-        const dimensionsWithSuffix = dimensions.map((dimension) => {
-            if (
-                [DIMENSION_TYPE_DATA_ELEMENT, DIMENSION_TYPE_PERIOD].includes(
-                    dimension.dimensionType || dimension.dimensionItemType
-                )
-            ) {
-                const duplicates = dimensions.filter(
-                    (d) =>
-                        d.dimensionId === dimension.dimensionId &&
-                        d !== dimension &&
-                        ((dimension.programId && d.programId) ||
-                            (dimension.programStageId && d.programStageId))
-                )
-
-                if (duplicates.length > 0) {
-                    const sameProgramId = duplicates.find(
-                        (dup) => dup.programId === dimension.programId
-                    )
-                    const thirdPartyDuplicates = duplicates
-                        .filter((dup) => dup.programId !== dimension.programId)
-                        .find((dpid) =>
-                            duplicates.find(
-                                (dup) =>
-                                    dup.programStageId !==
-                                        dpid.programStageId &&
-                                    dup.programId === dpid.programId
-                            )
-                        )
-
-                    if (sameProgramId || thirdPartyDuplicates) {
-                        dimension.suffix =
-                            metadata[dimension.programStageId].name
-                    } else {
-                        dimension.suffix = metadata[dimension.programId].name
-                    }
-                }
-            } else if (
-                // always suffix ou and statuses for TE
-                inputType === OUTPUT_TYPE_TRACKED_ENTITY &&
-                [
-                    DIMENSION_TYPE_ORGANISATION_UNIT,
-                    DIMENSION_TYPE_STATUS,
-                ].includes(
-                    dimension.dimensionType || dimension.dimensionItemType
-                ) &&
-                dimension.programId
-            ) {
-                dimension.suffix = metadata[dimension.programId].name
-            }
-
-            return dimension
-        })
-        return dimensionsWithSuffix
+        return dimensions
     }
 
-    return dimensions
+    return dimensions.map((dimension) => {
+        if (
+            [DIMENSION_TYPE_DATA_ELEMENT, DIMENSION_TYPE_PERIOD].includes(
+                dimension.dimensionType || dimension.dimensionItemType
+            )
+        ) {
+            const duplicates = dimensions.filter(
+                (d) =>
+                    d.dimensionId === dimension.dimensionId &&
+                    d !== dimension &&
+                    ((dimension.programId && d.programId) ||
+                        (dimension.programStageId && d.programStageId))
+            )
+
+            if (duplicates.length > 0) {
+                const sameProgramId = duplicates.find(
+                    (dup) => dup.programId === dimension.programId
+                )
+                const thirdPartyDuplicates = duplicates
+                    .filter((dup) => dup.programId !== dimension.programId)
+                    .find((dpid) =>
+                        duplicates.find(
+                            (dup) =>
+                                dup.programStageId !== dpid.programStageId &&
+                                dup.programId === dpid.programId
+                        )
+                    )
+
+                if (sameProgramId || thirdPartyDuplicates) {
+                    dimension.suffix = metadata[dimension.programStageId].name
+                } else {
+                    dimension.suffix = metadata[dimension.programId].name
+                }
+            }
+        } else if (
+            // always suffix ou and statuses for TE
+            inputType === OUTPUT_TYPE_TRACKED_ENTITY &&
+            [DIMENSION_TYPE_ORGANISATION_UNIT, DIMENSION_TYPE_STATUS].includes(
+                dimension.dimensionType || dimension.dimensionItemType
+            ) &&
+            dimension.programId
+        ) {
+            dimension.suffix = metadata[dimension.programId].name
+        }
+
+        return dimension
+    })
 }
