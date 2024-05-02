fe29545e0a7f6a38a8ff5b0460ecb4376f9ea1c3
Jen Jones Arnesen <jennifer@dhis2.org>
2024-05-02 10:12:45 +0200

    chore: consolidate to 2 tests and insert failure for testing

--- cypress/integration/smoke.cy.js
+++ cypress/integration/smoke.cy.js
@@ -4,8 +4,31 @@ import { goToStartPage } from '../helpers/startScreen.js'
 import { EXTENDED_TIMEOUT } from '../support/util.js'
 
 describe('Smoke Test', () => {
-    it('loads', () => {
+    it('app loads with correct system and user settings', () => {
+        cy.intercept('**userSettings**').as('userSettings')
+        cy.intercept('**systemSettings**').as('systemSettings')
+
         goToStartPage()
+        cy.log('Check user settings')
+        cy.wait('@userSettings').then((interception) => {
+            cy.log('userSettings', interception.response.body)
+            expect(interception.response.body.keyUiLocale).to.equal('en')
+            expect(
+                interception.response.body.keyAnalysisDisplayProperty
+            ).to.equal('name')
+        })
+        cy.log('Check system settings')
+        cy.wait('@systemSettings').then((interception) => {
+            cy.log('systemsettings', interception.response.body)
+            expect(
+                interception.response.body.keyAnalysisRelativePeriod
+            ).to.equal('LAST_12_MONTHS')
+            expect(
+                interception.response.body.keyAnalysisDigitGroupSeparator
+            ).to.equal('COMMA')
+        })
+
+        cy.log('Check app is loaded')
         cy.contains('Getting started', EXTENDED_TIMEOUT).should('be.visible')
         cy.title().should('equal', 'Line Listing | DHIS2')
     })
@@ -16,24 +39,5 @@ describe('Smoke Test', () => {
         cy.getBySel('titlebar', EXTENDED_TIMEOUT)
             .should('be.visible')
             .and('contain', TEST_AO.name)
-    })
-
-    it('system and user settings are correct', () => {
-        cy.intercept('**userSettings**').as('userSettings')
-        cy.intercept('**systemSettings**').as('systemSettings')
-        goToStartPage()
-        cy.wait('@userSettings').then((interception) => {
-            cy.log('userSettings', interception.response.body)
-            expect(interception.response.body.keyUiLocale).to.equal('en')
-            expect(
-                interception.response.body.keyAnalysisDisplayProperty
-            ).to.equal('name')
-        })
-        cy.wait('@systemSettings').then((interception) => {
-            cy.log('systemsettings', interception.response.body)
-            expect(
-                interception.response.body.keyAnalysisRelativePeriod
-            ).to.equal('LAST_12_MONTHS')
-        })
     })
 })
