bbda06fdbaf624304722c0633149d0f3ad079b76
Martin Ohlson <martin@moid.se>
2024-05-27 15:42:08 +0200

    test: update tests for getExcludedTags to min version 2.39, take 2

--- src/__tests__/cypressGetExcludedTags.spec.js
+++ src/__tests__/cypressGetExcludedTags.spec.js
@@ -3,86 +3,86 @@ import { getExcludedTags } from '../../cypress/plugins/excludeByVersionTags.js'
 describe('get excluded Cypress tags', () => {
     test('instanceVersion 2.39', () => {
         expect(getExcludedTags('2.39')).toEqual([
-            '<=38',
-            '<38',
             '<39',
             '>39',
             '>=40',
             '>40',
             '>=41',
+            '>41',
+            '>=42',
         ])
     })
 
     test('instanceVersion 2.40', () => {
         expect(getExcludedTags('2.40')).toEqual([
-            '<=38',
-            '<38',
             '<=39',
             '<39',
             '<40',
             '>40',
             '>=41',
+            '>41',
+            '>=42',
         ])
     })
 
     test('instanceVersion 2.41-SNAPSHOT', () => {
         expect(getExcludedTags('2.41-SNAPSHOT')).toEqual([
-            '<=38',
-            '<38',
             '<=39',
             '<39',
-            '<40',
             '<=40',
+            '<40',
             '<41',
+            '>41',
+            '>=42',
         ])
     })
 
     test('instanceVersion dev', () => {
         expect(getExcludedTags('dev')).toEqual([
-            '<=38',
-            '<38',
             '<=39',
             '<39',
-            '<40',
             '<=40',
+            '<40',
             '<41',
+            '<=41',
+            '<42',
         ])
     })
 
     // unexpected argument forms
     test('instanceVersion 39', () => {
         expect(getExcludedTags('39')).toEqual([
-            '<38',
-            '>38',
-            '>=39',
+            '<39',
             '>39',
             '>=40',
             '>40',
             '>=41',
+            '>41',
+            '>=42',
         ])
     })
 
     test('instanceVersion 39-SNAPSHOT.2', () => {
         expect(getExcludedTags('39-SNAPSHOT.2')).toEqual([
-            '<38',
-            '>38',
-            '>=39',
+            '<39',
             '>39',
             '>=40',
             '>40',
             '>=41',
+            '>41',
+            '>=42',
         ])
     })
 
     test('instanceVersion Dev', () => {
         expect(getExcludedTags('Dev')).toEqual([
-            '<=38',
-            '<38',
             '<=39',
             '<39',
-            '<40',
             '<=40',
+            '<40',
             '<41',
+            '<=41',
+            '<42',
         ])
     })
 
