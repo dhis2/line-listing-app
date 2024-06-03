c1e35cfe566190c42590ab67644deb1d02de32fe
HendrikThePendric <hendrik@dhis2.org>
2024-06-03 15:57:51 +0200

    feat: prevent sidebar from becoming to narrow or extend past the end of the window

--- src/components/MainSidebar/useResizableAccesorySidebar.js
+++ src/components/MainSidebar/useResizableAccesorySidebar.js
@@ -1,7 +1,8 @@
 import { useCallback, useMemo, useState } from 'react'
 
 const DEFAULT_WIDTH = 400
-// const MIN_WIDTH = 180
+const MIN_WIDTH = 180
+const MIN_PX_AT_END = 50
 
 export const useResizableAccessorySidebar = (isHidden) => {
     const [isResizing, setIsResizing] = useState(false)
@@ -27,15 +28,30 @@ export const useResizableAccessorySidebar = (isHidden) => {
     )
 
     const onResizeHandleMouseDown = useCallback((event) => {
-        let baseX = event.pageX
-        let deltaX = 0
-        const onMouseMove = (event) => {
-            setIsResizing(true)
-            deltaX = event.pageX - baseX
-            baseX = event.pageX
-            setWidth((currentWidth) => currentWidth + deltaX)
-        }
+        setIsResizing(true)
+        const startPageX = event.pageX
+        let startWidth = undefined
 
+        const onMouseMove = (event) => {
+            setWidth((currentWidth) => {
+                if (typeof startWidth === 'undefined') {
+                    startWidth = currentWidth - event.movementX
+                }
+
+                const virtualWidth = startWidth + (event.pageX - startPageX)
+
+                if (
+                    virtualWidth <= MIN_WIDTH ||
+                    event.pageX >= window.innerWidth - MIN_PX_AT_END
+                ) {
+                    return currentWidth
+                } else {
+                    return virtualWidth
+                }
+            })
+        }
+        /* Use the window as event target to avoid issues when the browser lags behind
+         * and the drag handle temporarily loses its hover state  */
         window.addEventListener('mousemove', onMouseMove)
         window.addEventListener(
             'mouseup',
