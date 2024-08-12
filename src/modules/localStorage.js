c0b0a0f49c0f9296c7d98e7498fe7c9298caefe3
HendrikThePendric <hendrik@dhis2.org>
2024-08-12 15:20:15 +0200

    fix: sanitize width before reading and writing to local storage

--- src/modules/localStorage.js
+++ src/modules/localStorage.js
@@ -2,12 +2,16 @@ import { ACCESSORY_PANEL_DEFAULT_WIDTH } from './accessoryPanelConstants.js'
 
 export const STORAGE_KEY = 'dhis2.line-listing.accessoryPanelWidth'
 
+const sanitizeWidth = (width) => {
+    const parsedWidth = parseInt(width)
+    return Number.isInteger(parsedWidth)
+        ? parsedWidth
+        : ACCESSORY_PANEL_DEFAULT_WIDTH
+}
+
 export const getUserSidebarWidthFromLocalStorage = () =>
-    parseInt(
-        window.localStorage.getItem(STORAGE_KEY) ??
-            ACCESSORY_PANEL_DEFAULT_WIDTH
-    )
+    sanitizeWidth(window.localStorage.getItem(STORAGE_KEY))
 
 export const setUserSidebarWidthToLocalStorage = (width) => {
-    window.localStorage.setItem(STORAGE_KEY, width)
+    window.localStorage.setItem(STORAGE_KEY, sanitizeWidth(width))
 }
