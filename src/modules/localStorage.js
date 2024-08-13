625ae09715371491ae4856579f8dc8ce94f04893
HendrikThePendric <hendrik@dhis2.org>
2024-08-13 10:43:30 +0200

    fix: ensure width is within lower and upper bound in a centralized place

--- src/modules/localStorage.js
+++ src/modules/localStorage.js
@@ -1,12 +1,33 @@
-import { ACCESSORY_PANEL_DEFAULT_WIDTH } from './accessoryPanelConstants.js'
+import {
+    ACCESSORY_PANEL_DEFAULT_WIDTH,
+    ACCESSORY_PANEL_MIN_PX_AT_END,
+    ACCESSORY_PANEL_MIN_WIDTH,
+    PRIMARY_PANEL_WIDTH,
+} from './accessoryPanelConstants.js'
 
 export const STORAGE_KEY = 'dhis2.line-listing.accessoryPanelWidth'
 
 const sanitizeWidth = (width) => {
-    const parsedWidth = parseInt(width)
-    return Number.isInteger(parsedWidth)
-        ? parsedWidth
-        : ACCESSORY_PANEL_DEFAULT_WIDTH
+    const maxAvailableWidth =
+        window.innerWidth -
+        (PRIMARY_PANEL_WIDTH + ACCESSORY_PANEL_MIN_PX_AT_END)
+    let sanitizedWidth = parseInt(width)
+
+    if (!Number.isInteger(sanitizedWidth)) {
+        sanitizedWidth = ACCESSORY_PANEL_DEFAULT_WIDTH
+    }
+
+    // First enforce upper bound
+    if (sanitizedWidth > maxAvailableWidth) {
+        sanitizedWidth = maxAvailableWidth
+    }
+
+    // Then enforce lower bound, so lower bound takes precedence
+    if (sanitizedWidth < ACCESSORY_PANEL_MIN_WIDTH) {
+        sanitizedWidth = ACCESSORY_PANEL_MIN_WIDTH
+    }
+
+    return sanitizedWidth
 }
 
 export const getUserSidebarWidthFromLocalStorage = () =>
