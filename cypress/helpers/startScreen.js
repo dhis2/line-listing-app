0142e586fbe4f513441e69427f5125aa613c41e0
Adel Bensaad <adel@dhis2.org>
2024-09-23 08:18:09 +0100

    chore: add logging to debug page state

--- cypress/helpers/startScreen.js
+++ cypress/helpers/startScreen.js
@@ -1,11 +1,36 @@
 import { EXTENDED_TIMEOUT } from '../support/util.js'
 
-export const goToStartPage = (skipEval) => {
-    cy.visit('/', EXTENDED_TIMEOUT).log(Cypress.env('dhis2BaseUrl'))
-    if (!skipEval) {
-        expectStartScreenToBeVisible()
-    }
+// Helper function to log page state and check if specific content is present
+const logPageState = () => {
+    cy.url().then((url) => {
+        cy.log(`Current URL: ${url}`)
+    })
+
+    cy.get('body').then((body) => {
+        const bodyText = body.text()
+        cy.log(`Page Body Content: ${bodyText.slice(0, 500)}...`)
+    })
 }
 
-export const expectStartScreenToBeVisible = () =>
-    cy.contains('Getting started', EXTENDED_TIMEOUT).should('be.visible')
+export const goToStartPage = (skipEval) => {
+    cy.visit('/', EXTENDED_TIMEOUT).then(() => {
+        cy.log(`Visiting the base URL: ${Cypress.env('dhis2BaseUrl')}`)
+        logPageState()
+
+        if (!skipEval) {
+            expectStartScreenToBeVisible()
+        }
+    })
+}
+
+export const expectStartScreenToBeVisible = () => {
+    cy.contains('Getting started', EXTENDED_TIMEOUT)
+        .should('be.visible')
+        .then(() => {
+            cy.log('Confirmed: "Getting started" is visible')
+        })
+        .catch(() => {
+            cy.log('Failed to find "Getting started" content')
+            logPageState()
+        })
+}
