7ac589d11891ebd0fb66a3f7716a30976a64045d
HendrikThePendric <hendrik@dhis2.org>
2025-02-26 16:56:57 +0100

    feat: use OrgUnitDimension component for OrgUnitCondition

--- src/components/Dialogs/Conditions/OrgUnitCondition.js
+++ src/components/Dialogs/Conditions/OrgUnitCondition.js
@@ -1,12 +1,12 @@
-import { useCachedDataQuery } from '@dhis2/analytics'
-import { OrganisationUnitTree } from '@dhis2/ui'
+import { OrgUnitDimension, useCachedDataQuery } from '@dhis2/analytics'
 import PropTypes from 'prop-types'
-import React from 'react'
+import React, { useCallback, useMemo } from 'react'
 import { connect } from 'react-redux'
 import { acAddMetadata } from '../../../actions/metadata.js'
 import { acAddParentGraphMap } from '../../../actions/ui.js'
-import { OPERATOR_EQUAL } from '../../../modules/conditions.js'
+import { OPERATOR_IN } from '../../../modules/conditions.js'
 import { getOuPath, removeLastPathSegment } from '../../../modules/orgUnit.js'
+import { DERIVED_USER_SETTINGS_DISPLAY_NAME_PROPERTY } from '../../../modules/userSettings.js'
 import { sGetMetadata } from '../../../reducers/metadata.js'
 import { sGetUiParentGraphMap } from '../../../reducers/ui.js'
 
@@ -18,57 +18,83 @@ const OrgUnitCondition = ({
     metadata,
     parentGraphMap,
 }) => {
-    const { rootOrgUnits } = useCachedDataQuery()
-    const parts = condition.split(':')
-    const value = parts[1]?.length && parts[1]
+    const { rootOrgUnits, currentUser } = useCachedDataQuery()
+    const selected = useMemo(() => {
+        const idsString = condition.split(':')?.[1] ?? ''
 
-    const setValues = (item) => {
-        if (item.checked) {
-            const forMetadata = {}
-            const forParentGraphMap = {}
-
-            forMetadata[item.id] = {
-                id: item.id,
-                name: item.name || item.displayName,
-                displayName: item.displayName,
-            }
-
-            if (item.path) {
-                const path = removeLastPathSegment(item.path)
-
-                forParentGraphMap[item.id] =
-                    path === `/${item.id}` ? '' : path.replace(/^\//, '')
-            }
-
-            addMetadata(forMetadata)
-            addParentGraphMap(forParentGraphMap)
-            onChange(`${OPERATOR_EQUAL}:${item.id}`)
-        } else {
-            onChange('')
+        if (idsString.length === 0) {
+            return []
         }
-    }
 
-    const selected = value
-        ? {
-              id: value,
-              name: metadata[value].displayName || metadata[value].name,
-              path: getOuPath(value, metadata, parentGraphMap),
-          }
-        : {}
+        return idsString.split(';').map((id) => {
+            const item = {
+                id,
+                name: metadata[id].displayName || metadata[id].name,
+            }
+            const isOuTreeItem =
+                !id.startsWith('LEVEL') &&
+                !id.startsWith('OU_GROUP') &&
+                !id.startsWith('USER_ORGUNIT')
 
+            if (isOuTreeItem) {
+                item.path = getOuPath(id, metadata, parentGraphMap)
+            }
+
+            return item
+        })
+    }, [condition, metadata, parentGraphMap])
+    const onSelect = useCallback(
+        ({ items }) => {
+            if (items.length === 0) {
+                onChange('')
+            } else {
+                const { forMetadata, forParentGraphMap, itemIds } =
+                    items.reduce(
+                        (acc, item) => {
+                            acc.itemIds.push(item.id)
+                            acc.forMetadata[item.id] = {
+                                id: item.id,
+                                name: item.name || item.displayName,
+                                displayName: item.displayName,
+                            }
+
+                            if (item.path) {
+                                const path = removeLastPathSegment(item.path)
+
+                                acc.forParentGraphMap[item.id] =
+                                    path === `/${item.id}`
+                                        ? ''
+                                        : path.replace(/^\//, '')
+                            }
+
+                            return acc
+                        },
+                        { forMetadata: {}, forParentGraphMap: {}, itemIds: [] }
+                    )
+                addMetadata(forMetadata)
+                addParentGraphMap(forParentGraphMap)
+                onChange(`${OPERATOR_IN}:${itemIds.join(';')}`)
+            }
+        },
+        [addMetadata, addParentGraphMap, onChange]
+    )
     const roots = rootOrgUnits.map((rootOrgUnit) => rootOrgUnit.id)
 
     return (
-        <OrganisationUnitTree
+        <OrgUnitDimension
+            selected={selected}
             roots={roots}
-            initiallyExpanded={[
-                ...(roots.length === 1 ? [`/${roots[0]}`] : []),
-                selected?.path?.substring(0, selected.path.lastIndexOf('/')),
-            ].filter((path) => path)}
-            selected={selected.path && [selected.path]}
-            onChange={(item) => setValues(item)}
-            dataTest="org-unit-tree"
-            singleSelection
+            displayNameProp={
+                currentUser.settings[
+                    DERIVED_USER_SETTINGS_DISPLAY_NAME_PROPERTY
+                ]
+            }
+            onSelect={onSelect}
+            // Not sure we need to set any of the props below
+            hideGroupSelect={false}
+            hideLevelSelect={false}
+            hideUserOrgUnits={false}
+            warning={undefined}
         />
     )
 }
