7f39aaedfe999d9f36d2a6098cb79d2febac53b0
HendrikThePendric <hendrik@dhis2.org>
2025-02-27 10:45:09 +0100

    fix: feature-toggle old and new OrgUnitCondition component on v42

--- src/components/Dialogs/Conditions/OrgUnitCondition.js
+++ src/components/Dialogs/Conditions/OrgUnitCondition.js
@@ -1,16 +1,81 @@
 import { OrgUnitDimension, useCachedDataQuery } from '@dhis2/analytics'
+import { useConfig } from '@dhis2/app-runtime'
+import { OrganisationUnitTree } from '@dhis2/ui'
 import PropTypes from 'prop-types'
 import React, { useCallback, useMemo } from 'react'
 import { connect } from 'react-redux'
 import { acAddMetadata } from '../../../actions/metadata.js'
 import { acAddParentGraphMap } from '../../../actions/ui.js'
-import { OPERATOR_IN } from '../../../modules/conditions.js'
+import { OPERATOR_EQUAL, OPERATOR_IN } from '../../../modules/conditions.js'
 import { getOuPath, removeLastPathSegment } from '../../../modules/orgUnit.js'
 import { DERIVED_USER_SETTINGS_DISPLAY_NAME_PROPERTY } from '../../../modules/userSettings.js'
 import { sGetMetadata } from '../../../reducers/metadata.js'
 import { sGetUiParentGraphMap } from '../../../reducers/ui.js'
 
-const OrgUnitCondition = ({
+const OrgUnitConditionMaxVersionV41 = ({
+    addMetadata,
+    addParentGraphMap,
+    condition,
+    onChange,
+    metadata,
+    parentGraphMap,
+}) => {
+    const { rootOrgUnits } = useCachedDataQuery()
+    const parts = condition.split(':')
+    const value = parts[1]?.length && parts[1]
+
+    const setValues = (item) => {
+        if (item.checked) {
+            const forMetadata = {}
+            const forParentGraphMap = {}
+
+            forMetadata[item.id] = {
+                id: item.id,
+                name: item.name || item.displayName,
+                displayName: item.displayName,
+            }
+
+            if (item.path) {
+                const path = removeLastPathSegment(item.path)
+
+                forParentGraphMap[item.id] =
+                    path === `/${item.id}` ? '' : path.replace(/^\//, '')
+            }
+
+            addMetadata(forMetadata)
+            addParentGraphMap(forParentGraphMap)
+            onChange(`${OPERATOR_EQUAL}:${item.id}`)
+        } else {
+            onChange('')
+        }
+    }
+
+    const selected = value
+        ? {
+              id: value,
+              name: metadata[value].displayName || metadata[value].name,
+              path: getOuPath(value, metadata, parentGraphMap),
+          }
+        : {}
+
+    const roots = rootOrgUnits.map((rootOrgUnit) => rootOrgUnit.id)
+
+    return (
+        <OrganisationUnitTree
+            roots={roots}
+            initiallyExpanded={[
+                ...(roots.length === 1 ? [`/${roots[0]}`] : []),
+                selected?.path?.substring(0, selected.path.lastIndexOf('/')),
+            ].filter((path) => path)}
+            selected={selected.path && [selected.path]}
+            onChange={(item) => setValues(item)}
+            dataTest="org-unit-tree"
+            singleSelection
+        />
+    )
+}
+
+const OrgUnitConditionMinVersionV42 = ({
     addMetadata,
     addParentGraphMap,
     condition,
@@ -99,6 +164,16 @@ const OrgUnitCondition = ({
     )
 }
 
+const OrgUnitCondition = (props) => {
+    const config = useConfig()
+
+    return config.serverVersion.minor >= 42 ? (
+        <OrgUnitConditionMinVersionV42 {...props} />
+    ) : (
+        <OrgUnitConditionMaxVersionV41 {...props} />
+    )
+}
+
 OrgUnitCondition.propTypes = {
     condition: PropTypes.string.isRequired,
     onChange: PropTypes.func.isRequired,
@@ -107,10 +182,14 @@ OrgUnitCondition.propTypes = {
     metadata: PropTypes.object,
     parentGraphMap: PropTypes.object,
 }
-
 OrgUnitCondition.defaultProps = {
     rootOrgUnits: [],
 }
+
+OrgUnitConditionMaxVersionV41.propTypes = OrgUnitCondition.propTypes
+OrgUnitConditionMinVersionV42.propTypes = OrgUnitCondition.propTypes
+OrgUnitConditionMaxVersionV41.defaultProps = OrgUnitCondition.defaultProps
+OrgUnitConditionMinVersionV42.defaultProps = OrgUnitCondition.defaultProps
 
 const mapStateToProps = (state) => ({
     metadata: sGetMetadata(state),
