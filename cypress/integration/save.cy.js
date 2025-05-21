ff364775925a259ec4b05e0179f10bcb4554269b
Jen Jones Arnesen <jennifer@dhis2.org>
2025-05-21 14:56:41 +0200

    test: add subscriber checks

--- cypress/integration/save.cy.js
+++ cypress/integration/save.cy.js
@@ -20,16 +20,12 @@ import {
 import {
     deleteVisualization,
     openAOByName,
-    renameVisualization,
     resaveVisualization,
     saveVisualization,
     saveVisualizationAs,
 } from '../helpers/fileMenu.js'
 import { expectAxisToHaveDimension } from '../helpers/layout.js'
-import {
-    clickMenubarUpdateButton,
-    clickMenubarInterpretationsButton,
-} from '../helpers/menubar.js'
+import { clickMenubarUpdateButton } from '../helpers/menubar.js'
 import { selectFixedPeriod, selectRelativePeriod } from '../helpers/period.js'
 import { goToStartPage } from '../helpers/startScreen.js'
 import {
@@ -68,134 +64,6 @@ const deleteVisualizationWithUid = (uid) =>
         failOnStatusCode: false, // carry on even if the delete fails
     })
 
-describe('rename', () => {
-    it('replace existing name works correctly', () => {
-        const AO_NAME = `SAVE-${Date.now()}-RENAME`
-        const UPDATED_AO_NAME = AO_NAME + '-superduper'
-        setupTable()
-
-        // save
-        saveVisualization(AO_NAME)
-        expectAOTitleToContainExact(AO_NAME)
-        expectTableToBeVisible()
-
-        cy.intercept('PUT', '**/api/*/eventVisualizations/*').as('put-rename')
-        cy.intercept('GET', '**/api/*/eventVisualizations/*').as('get-rename')
-
-        // rename the AO, changing name only
-        renameVisualization(UPDATED_AO_NAME)
-
-        cy.wait('@put-rename')
-
-        // Get visualization calls: original vis and updated name and description
-        cy.get('@get-rename.all').should('have.length', 2)
-
-        cy.getBySel('dhis2-uicore-alertbar')
-            .contains('Rename successful')
-            .should('be.visible')
-        expectTableToBeVisible()
-        expectAOTitleToContainExact(UPDATED_AO_NAME)
-
-        cy.reload(true)
-
-        expectTableToBeVisible()
-        expectAOTitleToContainExact(UPDATED_AO_NAME)
-
-        deleteVisualization()
-    })
-
-    it('add and change and delete name and description', () => {
-        const AO_NAME = `SAVE-${Date.now()}-RENAME`
-        const AO_DESC = 'w/description'
-        const AO_DESC_UPDATED = AO_DESC + '-edited'
-        setupTable()
-
-        // save
-        saveVisualization(AO_NAME)
-        expectAOTitleToContainExact(AO_NAME)
-        expectTableToBeVisible()
-
-        cy.intercept('PUT', '**/api/*/eventVisualizations/*').as('put-rename')
-        // rename the AO, adding a description
-        renameVisualization(AO_NAME, AO_DESC)
-        cy.wait('@put-rename')
-
-        clickMenubarInterpretationsButton()
-        cy.getBySel('details-panel').should('be.visible')
-        cy.getBySel('details-panel').containsExact(AO_DESC)
-        clickMenubarInterpretationsButton()
-
-        expectTableToBeVisible()
-
-        cy.intercept('PUT', '**/api/*/eventVisualizations/*').as('put-rename2')
-        // rename the AO, replacing the description
-        renameVisualization(AO_NAME, AO_DESC_UPDATED)
-        cy.wait('@put-rename2')
-
-        clickMenubarInterpretationsButton()
-        cy.getBySel('details-panel').should('be.visible')
-        cy.getBySel('details-panel').containsExact(AO_DESC_UPDATED)
-        clickMenubarInterpretationsButton()
-
-        expectTableToBeVisible()
-
-        cy.intercept('PUT', '**/api/*/eventVisualizations/*').as('put-rename3')
-        // now enter empty strings for the name and description
-        renameVisualization('', '')
-        cy.wait('@put-rename3')
-
-        clickMenubarInterpretationsButton()
-        cy.getBySel('details-panel').should('be.visible')
-        cy.getBySel('details-panel').containsExact('No description')
-        clickMenubarInterpretationsButton()
-
-        cy.reload(true)
-
-        // title is not deleted
-        cy.getBySel('titlebar').containsExact(AO_NAME)
-        clickMenubarInterpretationsButton()
-        cy.getBySel('details-panel').should('be.visible')
-        // description was successfully deleted
-        cy.getBySel('details-panel').contains('No description')
-        clickMenubarInterpretationsButton()
-
-        deleteVisualization()
-    })
-
-    it('handles failure when renaming', () => {
-        const AO_NAME = `SAVE-${Date.now()}-RENAME`
-        const UPDATED_AO_NAME = AO_NAME + '-superduper'
-        setupTable()
-
-        // save
-        saveVisualization(AO_NAME)
-        expectAOTitleToContainExact(AO_NAME)
-        expectTableToBeVisible()
-
-        cy.intercept('PUT', '**/api/*/eventVisualizations/*', {
-            statusCode: 409,
-        }).as('put-rename')
-
-        // rename the AO, changing name only
-        renameVisualization(UPDATED_AO_NAME)
-
-        cy.wait('@put-rename')
-
-        cy.getBySel('dhis2-uicore-alertbar')
-            .contains('Rename failed')
-            .should('be.visible')
-        expectTableToBeVisible()
-        expectAOTitleToContainExact(AO_NAME)
-
-        cy.reload(true)
-
-        expectTableToBeVisible()
-        expectAOTitleToContainExact(AO_NAME)
-
-        deleteVisualization()
-    })
-})
-
 describe('save', () => {
     it('new AO with name saves correctly (event)', () => {
         const AO_NAME = `SAVE-${Date.now()}-EVENT`
@@ -214,15 +82,72 @@ describe('save', () => {
                 cy.wrap(uid).as('firstSavedUid')
             })
 
+        // subscribe to the AO
+        cy.getBySel('dhis2-analytics-interpretationsanddetailstoggler').click()
+        cy.intercept(
+            'POST',
+            /\/api\/\d+\/eventVisualizations\/\w+\/subscriber/,
+            (req) => {
+                req.continue((res) => {
+                    expect([200, 201]).to.include(res.statusCode)
+                })
+            }
+        ).as('post-subscriber')
+        cy.get('button').contains('Subscribe').should('be.visible').click()
+        cy.wait('@post-subscriber')
+        cy.get('button').contains('Unsubscribe').should('be.visible')
+        cy.getBySel('dhis2-analytics-interpretationsanddetailstoggler').click()
+        cy.contains('About this line list').should('not.exist')
+
+        // check first GET request fetches subscribers
+        cy.intercept(
+            'GET',
+            /\/api\/\d+\/eventVisualizations\/\w+\?fields=subscribers/
+        ).as('get-subscribers')
+
+        // check the save request contains subscribers
+        cy.intercept(
+            'PUT',
+            /\/api\/\d+\/eventVisualizations\/\w+(\?.*)?/,
+            (req) => {
+                expect(req.body).to.have.property('subscribers')
+            }
+        ).as('put-save')
+
+        // click update to enable Save
+        clickMenubarUpdateButton()
+        resaveVisualization()
+        cy.wait('@get-subscribers')
+        cy.wait('@put-save')
+
         // open AO by name
         goToStartPage()
         openAOByName(AO_NAME)
         expectTableToBeVisible()
 
+        // check user is still subscribed
+        cy.getBySel('dhis2-analytics-interpretationsanddetailstoggler').click()
+        cy.get('button').contains('Unsubscribe').should('be.visible')
+        cy.getBySel('dhis2-analytics-interpretationsanddetailstoggler').click()
+        cy.contains('About this line list').should('not.exist')
+
         // save as with name change
+        cy.intercept(
+            'POST',
+            /\/api\/\d+\/eventVisualizations(\?.*)?/,
+            (req) => {
+                expect(req.body).to.not.have.property('subscribers')
+            }
+        ).as('post-saveas')
+
         saveVisualizationAs(UPDATED_AO_NAME)
+        cy.wait('@post-saveas')
         expectAOTitleToContainExact(UPDATED_AO_NAME)
         expectTableToBeVisible()
+
+        cy.getBySel('dhis2-analytics-interpretationsanddetailstoggler').click()
+        cy.contains('About this line list').should('be.visible')
+        cy.get('button').contains('Unsubscribe').should('not.exist')
 
         cy.url()
             .should('match', uidRe)
