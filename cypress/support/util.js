2ebd906ff250df6f89b01024b3b5c04d49bd0eb7
Edoardo Sabadelli <edoardo@dhis2.org>
2025-06-25 10:27:57 +0200

    test: use dhis2InstanceVersion for api version
    The GHA workflow is setting this variable with the api version.

--- cypress/support/util.js
+++ cypress/support/util.js
@@ -2,11 +2,17 @@ export const EXTENDED_TIMEOUT = { timeout: 25000 }
 
 export const getApiBaseUrl = () => {
     const baseUrl = Cypress.env('dhis2BaseUrl') || ''
-    const apiVersion = Cypress.env('dhis2ApiVersion') || ''
+    const apiVersion = Cypress.env('dhis2InstanceVersion') || ''
 
     if (!baseUrl) {
         throw new Error(
             'No `dhis2BaseUrl` found. Please make sure to add it to `cypress.env.json`'
+        )
+    }
+
+    if (!apiVersion) {
+        throw new Error(
+            'No `dhis2InstanceVersion` found. Please make sure to add it to `cypress.env.json` and to only contain the api version'
         )
     }
 
