bde2ec1dd263f0b0254dfa6f028763be3d8a11e9
Edoardo Sabadelli <edoardo@dhis2.org>
2025-06-26 11:53:08 +0200

    chore: update cli-style and remove rule from eslintrc

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,7 +2,4 @@ const { config } = require('@dhis2/cli-style')
 
 module.exports = {
     extends: [config.eslintReact, 'plugin:cypress/recommended'],
-    rules: {
-        'react/no-object-type-as-default-prop': 'error',
-    },
 }
