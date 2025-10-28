ff646a1d9d15eaaf5457fc14124d32c9959f6642
Joseph John Aas Cooper <33054985+cooper-joe@users.noreply.github.com>
2025-10-28 15:01:15 +0100

    fix: toolbar actions

--- src/components/Toolbar/Toolbar.jsx
+++ src/components/Toolbar/Toolbar.jsx
@@ -1,21 +1,63 @@
 import { Toolbar as AnalyticsToolbar } from '@dhis2/analytics'
 import PropTypes from 'prop-types'
-import React from 'react'
-
+import React, { useState } from 'react'
+import { useSelector } from 'react-redux'
+import { Button, IconShare16, IconDownload16, SharingDialog } from '@dhis2/ui'
+import { sGetCurrent } from '../../reducers/current.js'
 import { InterpretationsAndDetailsToggler } from './InterpretationsAndDetailsToggler.jsx'
 import { MenuBar } from './MenuBar.jsx'
 import { UpdateButton } from './UpdateButton.jsx'
 import TitleBar from '../TitleBar/TitleBar.jsx'
+import styles from './Toolbar.module.css'
 
-export const Toolbar = ({ onFileMenuAction }) => (
-    <AnalyticsToolbar>
-        <UpdateButton />
-        <MenuBar onFileMenuAction={onFileMenuAction} />
-        <TitleBar />
+export const Toolbar = ({ onFileMenuAction }) => {
+    const [sharingDialogOpen, setSharingDialogOpen] = useState(false)
+    const current = useSelector(sGetCurrent)
 
-        {/* <InterpretationsAndDetailsToggler /> */}
-    </AnalyticsToolbar>
-)
+    const handleShareClick = () => {
+        setSharingDialogOpen(true)
+        onFileMenuAction()
+    }
+
+    const handleSharingDialogClose = () => {
+        setSharingDialogOpen(false)
+    }
+
+    return (
+        <>
+            <AnalyticsToolbar>
+                <UpdateButton />
+                <MenuBar onFileMenuAction={onFileMenuAction} />
+                <TitleBar />
+                <div className={styles.toolbarActions}>
+                    <Button
+                        dense
+                        small
+                        secondary
+                        onClick={handleShareClick}
+                        disabled={!current?.id}
+                    >
+                        <IconShare16 />
+                    </Button>
+                    <Button
+                        icon={<IconDownload16 />}
+                        dense
+                        small
+                        secondary
+                    ></Button>
+                </div>
+                {/* <InterpretationsAndDetailsToggler /> */}
+            </AnalyticsToolbar>
+            {sharingDialogOpen && current?.id && (
+                <SharingDialog
+                    id={current.id}
+                    type="eventVisualization"
+                    onClose={handleSharingDialogClose}
+                />
+            )}
+        </>
+    )
+}
 
 Toolbar.propTypes = {
     onFileMenuAction: PropTypes.func.isRequired,
