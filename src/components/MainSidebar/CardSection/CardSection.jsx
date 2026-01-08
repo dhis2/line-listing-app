c34f55c69891e76ea2ed50529095405cd0b35e35
Joseph John Aas Cooper <33054985+cooper-joe@users.noreply.github.com>
2026-01-08 11:33:58 +0100

    fix: styles

--- src/components/MainSidebar/CardSection/CardSection.jsx
+++ src/components/MainSidebar/CardSection/CardSection.jsx
@@ -5,26 +5,31 @@ import styles from './CardSection.module.css'
 
 const ChevronDown = () => (
     <svg
-        xmlns="http://www.w3.org/2000/svg"
-        viewBox="0 0 12 12"
         width="12"
         height="12"
-        fill="currentColor"
+        viewBox="0 0 12 12"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
     >
-        <path d="M6 8.825c-.2 0-.4-.1-.5-.2l-3.3-3.3c-.3-.3-.3-.8 0-1.1.3-.3.8-.3 1.1 0l2.7 2.7 2.7-2.7c.3-.3.8-.3 1.1 0 .3.3.3.8 0 1.1l-3.2 3.2c-.2.2-.4.3-.6.3Z" />
+        <path
+            d="M3.37575 4.75194C3.18135 4.41861 3.42175 4 3.80765 4L8.19163 4C8.57753 4 8.81796 4.41861 8.62352 4.75194L6.43154 8.5096C6.2386 8.8404 5.7607 8.8404 5.56776 8.5096L3.37575 4.75194Z"
+            fill="currentColor"
+        />
     </svg>
 )
 
 const ChevronRight = () => (
     <svg
-        xmlns="http://www.w3.org/2000/svg"
-        viewBox="0 0 12 12"
         width="12"
         height="12"
-        fill="currentColor"
-        style={{ transform: 'rotate(-90deg)' }}
+        viewBox="0 0 12 12"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
     >
-        <path d="M6 8.825c-.2 0-.4-.1-.5-.2l-3.3-3.3c-.3-.3-.3-.8 0-1.1.3-.3.8-.3 1.1 0l2.7 2.7 2.7-2.7c.3-.3.8-.3 1.1 0 .3.3.3.8 0 1.1l-3.2 3.2c-.2.2-.4.3-.6.3Z" />
+        <path
+            d="M5.00194 8.62401C4.66861 8.81841 4.25 8.57801 4.25 8.19211V3.80813C4.25 3.42223 4.66861 3.1818 5.00194 3.37624L8.7596 5.56822C9.0904 5.76116 9.0904 6.23906 8.7596 6.432L5.00194 8.62401Z"
+            fill="currentColor"
+        />
     </svg>
 )
 
@@ -56,6 +61,7 @@ const CardSection = ({
             className={cx(styles.header, {
                 [styles.expanded]: expanded,
                 [styles.disabled]: disabled,
+                [styles.hasSelections]: count > 0,
             })}
             data-test={`${dataTest}-header`}
         >
@@ -64,7 +70,13 @@ const CardSection = ({
                     <div className={styles.chevron}>
                         {expanded ? <ChevronDown /> : <ChevronRight />}
                     </div>
-                    <span className={styles.label}>{label}</span>
+                    <span
+                        className={cx(styles.label, {
+                            [styles.hasSelections]: count > 0,
+                        })}
+                    >
+                        {label}
+                    </span>
                 </div>
                 {subtitle && (
                     <span
@@ -86,10 +98,10 @@ const CardSection = ({
             )}
         </div>
         {!disabled && (
-            <div 
+            <div
                 className={cx(styles.content, {
                     [styles.collapsed]: !expanded,
-                })} 
+                })}
                 data-test={`${dataTest}-content`}
                 onClick={(e) => e.stopPropagation()}
             >
@@ -114,4 +126,3 @@ CardSection.propTypes = {
 }
 
 export { CardSection }
-
